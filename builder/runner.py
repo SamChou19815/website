ca8c2b27660a8b1085181f43dac68825368d056f
Sam Zhou <sam@developersam.com>
2019-10-01 23:00:49 -0400

    [builder] Refactor builder to separate diff check and build/deploy (#115)

---
+++ builder/runner.py
@@ -0,0 +1,35 @@
+import os
+import subprocess
+import shutil
+from .configuration import Project
+
+
+def build_workspace(project: Project, affected: bool) -> int:
+    workspace = project.workspace
+
+    if affected:
+        print(f"Building {workspace}...")
+        return subprocess.call(project.build_command.split())
+
+    # Create dummy file if it's not affected.
+    build_output = project.build_output
+    if os.path.exists(path=build_output):
+        shutil.rmtree(path=build_output)
+    os.mkdir(path=build_output)
+    message = f"{workspace} is not affected by this change."
+    with open(os.path.join(build_output, "README.txt"), "w") as no_change_file:
+        no_change_file.write(message)
+    print(message)
+    return 0
+
+
+def deploy_workspace(project: Project, affected: bool) -> None:
+    workspace = project.workspace
+
+    if not affected:
+        print(f"{workspace} is not updated.")
+        return
+
+    subprocess.check_call(project.build_command.split())
+    for one_deploy_command in project.deploy_command:
+        subprocess.check_call(one_deploy_command.split())
