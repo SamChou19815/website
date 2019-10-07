9985d7b600206ec7319a586f89dc862c7c0970cb
Sam <sam@developersam.com>
2019-10-06 22:56:36 -0400

    Finally fix syntax highlighter for types

--- sam-react-common/language.ts
+++ sam-react-common/language.ts
@@ -1,5 +1,32 @@
 import highlight, { HLJSStatic, IMode } from 'highlight.js';
 
+const simpleTypeMode: IMode = {
+  className: 'type',
+  begin: /[A-Z][A-Za-z0-9]*/,
+  end: ''
+};
+
+const simpleIdentifierAsFunctionCallMode: IMode = {
+  className: 'function-call',
+  begin: /[a-z][A-Za-z0-9]*/,
+  end: ''
+};
+
+const keywords = {
+  keyword: 'class val function method as public if then else match from',
+  literal: 'false true _',
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  built_in: 'unit int bool string this'
+};
+
+const createModeWithNested = (className: string, regex: RegExp, nested: IMode): IMode => ({
+  className, begin: regex, returnBegin: true, end: '', contains: [nested],
+});
+
+const createModeWithType = (className: string, regex: RegExp): IMode => createModeWithNested(className, regex, simpleTypeMode);
+
+const createModeWithFunctionCall = (className: string, regex: RegExp): IMode => createModeWithNested(className, regex, simpleIdentifierAsFunctionCallMode);
+
 export default (): void =>
   highlight.registerLanguage(
     'samlang',
@@ -8,24 +35,14 @@ export default (): void =>
         throw new Error('highlight is not defined.');
       }
       return {
-        keywords: {
-          keyword: 'class val function method as public if then else match import from',
-          literal: 'false true _',
-          // eslint-disable-next-line @typescript-eslint/camelcase
-          built_in: 'unit int bool string this panic'
-        },
+        keywords,
         contains: [
-          hljs.QUOTE_STRING_MODE,
-          hljs.C_LINE_COMMENT_MODE,
-          hljs.C_BLOCK_COMMENT_MODE,
-          hljs.NUMBER_MODE,
+          highlight.C_LINE_COMMENT_MODE,
+          highlight.C_BLOCK_COMMENT_MODE,
+          highlight.NUMBER_MODE,
+          highlight.QUOTE_STRING_MODE,
           {
-            className: 'type',
-            begin: '(?<![A-Za-z0-9])[A-Z][A-Za-z0-9]*',
-            end: ''
-          },
-          {
-            className: 'method',
+            className: 'function-call',
             begin: /::/,
             end: /\(/,
             excludeBegin: true,
@@ -33,8 +50,41 @@ export default (): void =>
           },
           {
             className: 'punctuations',
-            begin: /,|\[|]|::|=| |:/,
+            begin: /,|\[|]|::|=|->|:/,
             end: ''
+          },
+          {
+            className: 'import-part',
+            begin: /import\s*\{/,
+            end: /\}/,
+            keywords: 'import',
+            contains: [simpleTypeMode]
+          },
+          {
+            className: 'from-part',
+            begin: /from/,
+            end: /\s*(?=(import|class|$))/,
+            keywords: 'from',
+            contains: [simpleTypeMode]
+          },
+          {
+            className: 'type-parameters',
+            keywords,
+            begin: '<',
+            end: '>',
+            excludeBegin: true,
+            excludeEnd: true,
+            contains: [simpleTypeMode]
+          },
+          createModeWithFunctionCall('simple-function-call', /[a-z][A-Za-z0-9]*\s*\(/),
+          createModeWithType('variant-constructor', /[A-Z][A-Za-z0-9]*\s*\(/),
+          createModeWithType('class-function', /[A-Z][A-Za-z0-9]*\s*::/),
+          createModeWithType('class-name-with-type-parameter', /[A-Z][A-Za-z0-9]*\s*</),
+          createModeWithType('utility-class-name', /[A-Z][A-Za-z0-9]*\s*\{/),
+          {
+            className: 'type',
+            begin: /\s+[A-Z][A-Za-z0-9]*/,
+            end: '',
           }
         ]
       };
