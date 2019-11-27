cd3d11ed8e91f4f613e0ff281cdf160cb8a2b459
Sam Zhou <sam@developersam.com>
2019-11-26 19:01:05 -0500

    [builder] Ensure the builder only generates configuration (#195)
    In order to get the diff small, it gets rid of avoiding unnecessary build/deploy feature for now. It should be added back in a future diff.

--- builder/builder.py
+++ builder/builder.py
@@ -3,11 +3,7 @@ import os
 import sys
 
 from .workspace import validate_dependency_chain
-from .configuration import create_yarn_workspace_project
 from .generator import generate_workflows
-from .git import get_changed_paths, get_changed_paths_last_commit
-from .diff import is_affected, is_any_project_affected
-from .runner import install_workspace, build_workspace, deploy_workspace
 
 
 def _validate_dependencies(arguments: argparse.Namespace) -> None:
@@ -19,38 +15,6 @@ def _generate_workflows(arguments: argparse.Namespace) -> None:
     for yml_file, yml_file_content in generate_workflows():
         with open(os.path.join(output_directory, yml_file), "w") as output_file:
             output_file.write(yml_file_content)
-
-
-def _install_for_build_if_affected(arguments: argparse.Namespace) -> None:
-    changed_paths = get_changed_paths(
-        base_ref=arguments.base_ref, head_ref=arguments.head_ref
-    )
-    install_workspace(affected=is_any_project_affected(changed_paths=changed_paths))
-
-
-def _install_for_deploy_if_affected(arguments: argparse.Namespace) -> None:
-    install_workspace(
-        affected=is_any_project_affected(changed_paths=get_changed_paths_last_commit())
-    )
-
-
-def _build_if_affected(arguments: argparse.Namespace) -> None:
-    project = create_yarn_workspace_project(workspace=arguments.workspace)
-    changed_paths = get_changed_paths(
-        base_ref=arguments.base_ref, head_ref=arguments.head_ref
-    )
-    affected = is_affected(changed_paths=changed_paths, project=project)
-
-    build_workspace(project=project, affected=affected)
-
-
-def _deploy_if_affected(arguments: argparse.Namespace) -> None:
-    project = create_yarn_workspace_project(workspace=arguments.workspace)
-    affected = is_affected(
-        changed_paths=get_changed_paths_last_commit(), project=project
-    )
-
-    deploy_workspace(project=project, affected=affected)
 
 
 def main() -> bool:
@@ -69,33 +33,6 @@ def main() -> bool:
     generate_workflows_parser.add_argument(
         "--output-directory", default=".github/workflows"
     )
-
-    install_for_install_if_affected_parser = parsed_commands.add_parser(
-        name="install-for-build-if-affected"
-    )
-    install_for_install_if_affected_parser.set_defaults(
-        command=_install_for_build_if_affected
-    )
-    install_for_install_if_affected_parser.add_argument("--deploy", action="store_true")
-    install_for_install_if_affected_parser.add_argument("--base-ref", required=False)
-    install_for_install_if_affected_parser.add_argument("--head-ref", required=False)
-
-    install_for_deploy_if_affected_parser = parsed_commands.add_parser(
-        name="install-for-deploy-if-affected"
-    )
-    install_for_deploy_if_affected_parser.set_defaults(
-        command=_install_for_deploy_if_affected
-    )
-
-    build_if_affected_parser = parsed_commands.add_parser(name="build-if-affected")
-    build_if_affected_parser.set_defaults(command=_build_if_affected)
-    build_if_affected_parser.add_argument("--base-ref", required=True)
-    build_if_affected_parser.add_argument("--head-ref", required=True)
-    build_if_affected_parser.add_argument("--workspace", required=True)
-
-    deploy_if_affected_parser = parsed_commands.add_parser(name="deploy-if-affected")
-    deploy_if_affected_parser.set_defaults(command=_deploy_if_affected)
-    deploy_if_affected_parser.add_argument("--workspace", required=True)
 
     arguments = parser.parse_args()
 
--- builder/configuration.py
+++ builder/configuration.py
@@ -6,38 +6,22 @@ from .workspace import get_dependency_chain
 @dataclass
 class Project:
     workspace: str
-    build_command: str
-    deploy_command: str
-    build_output: str
     relevant_paths: Sequence[str]
 
 
 def _create_project(
-    workspace: str,
-    build_command: str,
-    deploy_command: str,
-    build_output: str,
-    additional_dependency_paths: Sequence[str],
+    workspace: str, additional_dependency_paths: Sequence[str]
 ) -> Project:
     relevant_paths: Sequence[str] = [
         *[f"{dependency}/**" for dependency in get_dependency_chain(workspace)],
         *additional_dependency_paths,
     ]
-    return Project(
-        workspace=workspace,
-        build_command=build_command,
-        deploy_command=deploy_command,
-        build_output=build_output,
-        relevant_paths=relevant_paths,
-    )
+    return Project(workspace=workspace, relevant_paths=relevant_paths)
 
 
 def create_yarn_workspace_project(workspace: str) -> Project:
     return _create_project(
         workspace=workspace,
-        build_command=f"yarn workspace {workspace} build",
-        deploy_command=f"yarn workspace {workspace} deploy",
-        build_output=f"{workspace}/build",
         additional_dependency_paths=["package.json", "yarn.lock", "configuration/**"],
     )
 
