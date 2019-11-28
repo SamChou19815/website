9997cedaa743a39581d16d3bf603896c012f2816
Sam <sam@developersam.com>
2019-11-27 20:50:45 -0500

    Style the `TerminalEmulator` to make it consistent with the rest of website

--- packages/www/src/components/TerminalEmulator/index.tsx
+++ packages/www/src/components/TerminalEmulator/index.tsx
@@ -1,5 +1,17 @@
 import React, { ReactElement } from 'react';
 import Terminal from 'react-console-emulator';
 import commands from './commands';
+import styles from './TerminalEmulator.module.css';
 
-export default (): ReactElement => <Terminal commands={commands} />;
+export default (): ReactElement => (
+  <Terminal
+    commands={commands}
+    welcomeMessage="A terminal for Developer Sam Web OS."
+    promptLabel="you@developersam.com:~$"
+    className={styles.Terminal}
+    contentClassName={styles.TextColor}
+    inputAreaClassName={styles.TextColor}
+    promptLabelClassName={styles.TextColor}
+    inputClassName={styles.TextColor}
+  />
+);
