c722d7f2766359cedb5aed3898217fcf5b7b43a3
Sam Zhou <sam@developersam.com>
2019-11-28 18:12:34 -0500

    [www][terminal] Add support for `ls` command (#222)

--- packages/www/src/filesystem/index.ts
+++ packages/www/src/filesystem/index.ts
@@ -1,5 +1,6 @@
 import initialState from './initial-state';
 import { currentDirectoryPath } from './path';
 import { changeDirectory } from './stack';
+import listFiles from './list-files';
 
-export { initialState, currentDirectoryPath, changeDirectory };
+export { initialState, currentDirectoryPath, changeDirectory, listFiles };
