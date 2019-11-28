0d35bb821013cd4f093770ca99eaa8a5f1a416a1
Sam <sam@developersam.com>
2019-11-27 20:23:58 -0500

    Reformat `TerminalEmulator/index.tsx`

--- packages/www/src/components/TerminalEmulator/index.tsx
+++ packages/www/src/components/TerminalEmulator/index.tsx
@@ -2,4 +2,4 @@ import React, { ReactElement } from 'react';
 import Terminal from 'react-console-emulator';
 import commands from './commands';
 
-export default (): ReactElement => <Terminal commands={commands} />
+export default (): ReactElement => <Terminal commands={commands} />;
