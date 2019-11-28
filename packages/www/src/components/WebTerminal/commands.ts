6a32012bc9b1e784b90aac9f31304d3d336c3e34
Sam <sam@developersam.com>
2019-11-28 17:19:23 -0500

    [www] Connect `pwd` command in filesystem to prod terminal

--- packages/www/src/components/WebTerminal/commands.ts
+++ packages/www/src/components/WebTerminal/commands.ts
@@ -1,9 +1,7 @@
 import { Commands } from 'react-console-emulator';
-import { initialState, changeDirectory } from '../../filesystem';
+import { initialState, currentDirectoryPath, changeDirectory } from '../../filesystem';
 
 let fileSystemState = initialState;
-
-const echo = (...inputs: string[]): string => inputs.join(' ');
 
 const cd = (path: string): string | null => {
   try {
@@ -17,10 +15,15 @@ const cd = (path: string): string | null => {
 const devSam = (): string =>
   `Copyright (C) 2015–${new Date().getFullYear()} Developer Sam. All rights reserved.`;
 
+const echo = (...inputs: string[]): string => inputs.join(' ');
+
+const pwd = (): string => currentDirectoryPath(fileSystemState);
+
 const commands: Commands = {
+  cd: { fn: cd, description: 'Change current directory.', usage: 'cd [path]' },
+  'dev-sam': { fn: devSam, description: 'You guess what it is.' },
   echo: { fn: echo, description: 'Print back the arguments.', usage: 'echo [foo] [bar] ...' },
-  cd: { fn: cd, description: 'Change current directory', usage: 'cd [path]' },
-  'dev-sam': { fn: devSam, description: 'You guess', usage: 'dev-sam' }
+  pwd: { fn: pwd, description: 'Print currrent absolute directory.' }
 };
 
 export default commands;
