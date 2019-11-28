2f0d13327acc0c91b6213cb44b8a6f17f7708a35
Sam Zhou <sam@developersam.com>
2019-11-27 20:27:55 -0500

    [www] Setup basic structure for terminal emulator (#209)

---
+++ packages/www/src/components/TerminalEmulator/index.tsx
@@ -0,0 +1,5 @@
+import React, { ReactElement } from 'react';
+import Terminal from 'react-console-emulator';
+import commands from './commands';
+
+export default (): ReactElement => <Terminal commands={commands} />;
