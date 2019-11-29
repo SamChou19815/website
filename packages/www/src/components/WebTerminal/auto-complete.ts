3128052a56a16d706f178ceffc6e050196a1dfb0
Sam <sam@developersam.com>
2019-11-29 17:23:15 -0500

    [www][terminal] Implement command autocompletion
    Auto-completing command is very straightforward: a simple trie would work. (although I didn't actually introduce one, since number of supported commands is bounded above by a small constant).
    Filename autocompletion might be more involved. Delay this until we have the infra ready.

---
+++ packages/www/src/components/WebTerminal/auto-complete.ts
@@ -0,0 +1,50 @@
+/**
+ * @param sources a list of strings that can be used to expand the prefix.
+ * @param prefix the prefix to expand. No whitespace should be in the string.
+ * @returns expanded prefix string, or `null` if autocompletion failed.
+ */
+export const autoCompleteOnePart = (sources: readonly string[], prefix: string): string | null => {
+  const matchedPrefix = sources.filter(source => source.startsWith(prefix));
+  if (matchedPrefix.length === 0) {
+    return null;
+  }
+  if (matchedPrefix.length === 1) {
+    return matchedPrefix[0];
+  }
+  let bestPrefix = prefix;
+  for (let i = prefix.length; ; i += 1) {
+    const characterSet = new Set<string>();
+    for (let j = 0; j < matchedPrefix.length; j += 1) {
+      const source = matchedPrefix[j];
+      if (i >= source.length) {
+        return bestPrefix;
+      }
+      characterSet.add(source.charAt(i));
+    }
+    if (characterSet.size < 1) {
+      throw new Error('Impossible!');
+    }
+    if (characterSet.size > 1) {
+      // Choosing either one means loss of generality. We stop here.
+      return bestPrefix;
+    }
+    bestPrefix += Array.from(characterSet.entries())[0][0];
+  }
+};
+
+/**
+ * @param sources a list of strings that can be used to expand the prefix.
+ * @param prefix the line to expand.
+ * @returns expanded line.
+ */
+const autoCompleteCommandLine = (sources: readonly string[], line: string): string => {
+  const parts = line.trim().split(' ');
+  if (parts.length === 0) {
+    return '';
+  }
+  const result = autoCompleteOnePart(sources, parts[parts.length - 1]);
+  const finalPart = result === null ? parts[parts.length - 1] : result;
+  return [...parts.slice(0, parts.length - 1), finalPart].join(' ').trim();
+};
+
+export default autoCompleteCommandLine;
