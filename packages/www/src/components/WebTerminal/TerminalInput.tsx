bd9ce500565fd82507f4691a4b31b8026bd5b764
Sam <sam@developersam.com>
2019-12-07 01:16:16 -0500

    [www][terminal] Factor out `TerminalInput` component

---
+++ packages/www/src/components/WebTerminal/TerminalInput.tsx
@@ -0,0 +1,51 @@
+import React, { ReactElement, KeyboardEvent, RefObject, useState } from 'react';
+import autoComplete from './auto-complete';
+import styles from './Terminal.module.css';
+
+type Props = {
+  readonly terminalInput: RefObject<HTMLInputElement>;
+  readonly onArrow: (arrow: 'up' | 'down') => string | null;
+  readonly onSubmit: (line: string) => void;
+};
+
+export default ({ terminalInput, onArrow, onSubmit }: Props): ReactElement => {
+  const [line, setLine] = useState('');
+
+  const onKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+    switch (event.key) {
+      case 'Enter':
+        setLine('');
+        onSubmit(line);
+        break;
+      case 'ArrowUp':
+      case 'ArrowDown': {
+        const result = onArrow(event.key === 'ArrowUp' ? 'up' : 'down');
+        if (result !== null) {
+          setLine(result);
+        }
+        break;
+      }
+      case 'Tab':
+        event.preventDefault();
+        setLine(currentLine => autoComplete(currentLine));
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div className={styles.TerminalInputArea}>
+      <span className={styles.TerminalPromptLabel}>$</span>
+      <input
+        ref={terminalInput}
+        className={styles.TerminalInput}
+        value={line}
+        onChange={event => setLine(event.currentTarget.value)}
+        onKeyDown={onKeyDown}
+        type="text"
+        autoComplete="off"
+      />
+    </div>
+  );
+};
--- packages/www/src/components/WebTerminal/history.ts
+++ packages/www/src/components/WebTerminal/history.ts
@@ -1,8 +1,8 @@
 /* eslint-disable prefer-destructuring */
-import { RefObject } from 'react';
 import { TerminalHistory } from './types';
 
 type Result = {
+  readonly value: string | null;
   readonly historyPosition: number | null;
   readonly previousHistoryPosition: number | null;
 };
@@ -11,61 +11,59 @@ export default (
   direction: 'up' | 'down',
   commandHistory: readonly TerminalHistory[],
   historyPosition: number | null,
-  previousHistoryPosition: number | null,
-  terminalInput: RefObject<HTMLInputElement>
+  previousHistoryPosition: number | null
 ): Result => {
   // Clean empty items and reverse order to ease position tracking.
   const history = Array.from(commandHistory)
     .filter(({ isCommand }) => isCommand)
+    .map(item => item.line)
     .reverse();
   const position = historyPosition;
   const previousPosition = previousHistoryPosition;
-  const termNode = terminalInput.current;
-  if (termNode == null) {
-    throw new Error();
-  }
 
   if (history.length === 0) {
-    return { historyPosition, previousHistoryPosition };
+    return { value: null, historyPosition, previousHistoryPosition };
   }
   // Only run if history is non-empty and in use
   switch (direction) {
     case 'up':
       if (position == null) {
         // If at no position, get most recent entry
-        termNode.value = history[0].line;
-        return { historyPosition: 0, previousHistoryPosition: null };
+        return { value: history[0], historyPosition: 0, previousHistoryPosition: null };
       }
       if (position + 1 === history.length) {
         // If the first entry will be reached on this press,
         // get it and decrement position by 1 to avoid confusing downscroll.
-        termNode.value = history[history.length - 1].line;
         return {
+          value: history[history.length - 1],
           historyPosition: history.length - 1,
           previousHistoryPosition: history.length - 2
         };
       }
       // Normal increment by one
-      termNode.value = history[position + 1].line;
-      return { historyPosition: position + 1, previousHistoryPosition: position };
+      return {
+        value: history[position + 1],
+        historyPosition: position + 1,
+        previousHistoryPosition: position
+      };
     case 'down':
       if (position == null || !history[position]) {
         // If at initial or out of range, clear (Unix-like behaviour)
-        termNode.value = '';
-        return { historyPosition: null, previousHistoryPosition: null };
+        return { value: '', historyPosition: null, previousHistoryPosition: null };
       }
       if (position - 1 === -1) {
-        // Clear because user pressed up once and is now pressing down again => clear or is reaching bottom
-        if (previousPosition === null || (position === 0 && previousPosition === 1)) {
-          termNode.value = '';
-        } else {
-          termNode.value = history[0].line;
-        }
-        return { historyPosition: null, previousHistoryPosition: null };
+        // Clear because user pressed up once and is now pressing down again =>
+        // clear or is reaching bottom
+        const value =
+          previousPosition === null || (position === 0 && previousPosition === 1) ? '' : history[0];
+        return { value, historyPosition: null, previousHistoryPosition: null };
       }
       // Normal decrement by one
-      termNode.value = history[position - 1].line;
-      return { historyPosition: position - 1, previousHistoryPosition: position };
+      return {
+        value: history[position - 1],
+        historyPosition: position - 1,
+        previousHistoryPosition: position
+      };
     default:
       throw new Error();
   }
