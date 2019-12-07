0632211baaeb9f055df7e5987582e86e691e611e
Sam Zhou <sam@developersam.com>
2019-12-07 01:52:58 -0500

    [www][terminal] Add more terminal tests (#254)

---
+++ packages/www/src/components/WebTerminal/commands.test.ts
@@ -0,0 +1,33 @@
+import commands from './commands';
+
+it('All commands have description', () => {
+  Array.from(Object.values(commands)).forEach(command => {
+    expect(command.description).toBeTruthy();
+  });
+});
+
+it('commands does not crash', () => {
+  commands.help.fn();
+  commands.cat.fn();
+  commands.cat.fn('README.md');
+  commands.cat.fn('ahhh!');
+  commands.cd.fn();
+  commands.cd.fn('top-secret');
+  commands.cd.fn('ahhh!');
+  commands['dev-sam'].fn();
+  expect(commands.echo.fn()).toBe('');
+  expect(commands.echo.fn('foo', 'bar')).toBe('foo bar');
+  commands.ls.fn();
+  commands.ls.fn('top-secret');
+  commands.ls.fn('ahh!!!');
+  commands.pwd.fn();
+  commands.pwd.fn('ahh!!!');
+  commands.timeline.fn();
+  commands.timeline.fn('--none');
+  commands.timeline.fn('--only', 'work');
+  commands.timeline.fn('--only', 'projects');
+  commands.timeline.fn('--only', 'events');
+  commands.timeline.fn('--only', 'random');
+  commands.timeline.fn('--only');
+  commands.timeline.fn('bad command');
+});
--- packages/www/src/components/WebTerminal/index.tsx
+++ packages/www/src/components/WebTerminal/index.tsx
@@ -1,17 +1,24 @@
 import React, { ReactElement, useState } from 'react';
-import WebTerminal from './Terminal';
+import Terminal from './Terminal';
 import Trigger from './Trigger';
 
-const memoizedTerminal = <WebTerminal />;
+const memoizedTerminal = <Terminal />;
+
+type Props = {
+  readonly shown: boolean;
+  readonly onTrigger: () => void;
+};
+
+export const WebTerminal = ({ shown, onTrigger }: Props): ReactElement => (
+  <>
+    <Trigger onClick={onTrigger} />
+    {shown && memoizedTerminal}
+  </>
+);
 
 export default (): ReactElement => {
   const [isTerminalShown, setIsTerminalShown] = useState(false);
   const toggle = (): void => setIsTerminalShown(isShown => !isShown);
 
-  return (
-    <>
-      <Trigger onClick={toggle} />
-      {isTerminalShown && memoizedTerminal}
-    </>
-  );
+  return <WebTerminal shown={isTerminalShown} onTrigger={toggle} />;
 };
