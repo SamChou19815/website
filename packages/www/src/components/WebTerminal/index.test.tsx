4f4ec3ae5a8d50111acfee0c83ea00dbb408a313
Sam <sam@developersam.com>
2019-12-07 21:44:40 -0500

    [www][terminal] Make test coverage green

--- packages/www/src/components/WebTerminal/index.test.tsx
+++ packages/www/src/components/WebTerminal/index.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { WebTerminal } from '.';
+import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import Default, { WebTerminal } from '.';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 const ignore = () => {};
@@ -13,4 +15,28 @@ it(`WebTerminal(shown) matches snapshot.`, () => {
 it(`WebTerminal(hidden) matches snapshot.`, () => {
   const tree = renderer.create(<WebTerminal shown={false} onTrigger={ignore} />).toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+it(`Default matches snapshot.`, () => {
+  const tree = renderer.create(<Default />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('Can trigger', () => {
+  const container = document.createElement('div');
+  document.body.appendChild(container);
+
+  act(() => {
+    ReactDOM.render(<Default />, container);
+  });
+  const buttonNode = document.querySelector('button') as HTMLButtonElement;
+
+  act(() => {
+    buttonNode.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+  });
+  act(() => {
+    buttonNode.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+  });
+
+  document.body.removeChild(container);
 });
