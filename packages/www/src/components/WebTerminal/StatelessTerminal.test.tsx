ef5083a9f66b131973ba76fbbe697c6288861373
Sam Zhou <sam@developersam.com>
2019-12-07 20:50:37 -0500

    [www][terminal] Snapshot test on `StatelessTerminal` (#258)

---
+++ packages/www/src/components/WebTerminal/StatelessTerminal.test.tsx
@@ -0,0 +1,30 @@
+import React, { ReactElement, useRef } from 'react';
+import renderer from 'react-test-renderer';
+import StatelessTerminal from './StatelessTerminal';
+import { TerminalHistory } from './types';
+
+const Wrapper = ({ history }: { readonly history: readonly TerminalHistory[] }): ReactElement => {
+  const terminalRoot = useRef<HTMLDivElement>(null);
+  const terminalInput = useRef<HTMLInputElement>(null);
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  const ignore = () => {};
+  return (
+    <StatelessTerminal
+      history={history}
+      terminalRoot={terminalRoot}
+      terminalInput={terminalInput}
+      focusTerminal={ignore}
+      onArrow={() => null}
+      processCommand={ignore}
+    />
+  );
+};
+
+it(`StatelessTerminal matches snapshot.`, () => {
+  const history = [
+    { isCommand: true, line: 'foo' },
+    { isCommand: false, line: 'bar' }
+  ];
+  const tree = renderer.create(<Wrapper history={history} />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
