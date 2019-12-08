5e3232dc43651fa479b763b7960fc3c4ecfb5be0
Sam Zhou <sam@developersam.com>
2019-12-07 19:39:52 -0500

    [www][terminal] Fully test history manager (#256)

--- packages/www/src/components/WebTerminal/history.test.ts
+++ packages/www/src/components/WebTerminal/history.test.ts
@@ -1,8 +1,41 @@
 import history from './history';
 
 it('Trivial cases return null.', () => {
-  expect(history('up', [], null, null)).toBeNull();
-  expect(history('down', [], null, null)).toBeNull();
-  expect(history('up', [{ isCommand: false, line: '' }], null, null)).toBeNull();
-  expect(history('down', [{ isCommand: false, line: '' }], null, null)).toBeNull();
+  expect(history('up', [], null)).toBeNull();
+  expect(history('down', [], null)).toBeNull();
+});
+
+it('arrow up works.', () => {
+  expect(history('up', ['foo', 'bar'], null)).toStrictEqual({ value: 'foo', historyPosition: 0 });
+  expect(history('up', ['foo', 'bar'], 0)).toStrictEqual({ value: 'bar', historyPosition: 1 });
+  expect(history('up', ['foo', 'bar'], 1)).toStrictEqual({ value: 'bar', historyPosition: 1 });
+});
+
+it('arrow down works', () => {
+  expect(history('down', ['foo', 'bar'], 1)).toStrictEqual({ value: 'foo', historyPosition: 0 });
+  expect(history('down', ['foo', 'bar'], 0)).toStrictEqual({ value: '', historyPosition: null });
+  expect(history('down', ['foo', 'bar'], null)).toStrictEqual({ value: '', historyPosition: null });
+});
+
+it('history integration test', () => {
+  const historyList = ['foo', 'bar'];
+  const result = history(
+    'down',
+    historyList,
+    history(
+      'down',
+      historyList,
+      history(
+        'down',
+        historyList,
+        history(
+          'up',
+          historyList,
+          history('up', historyList, history('up', historyList, null)?.historyPosition ?? null)
+            ?.historyPosition
+        )?.historyPosition
+      )?.historyPosition
+    )?.historyPosition
+  );
+  expect(result?.historyPosition).toBeNull();
 });
--- packages/www/src/components/WebTerminal/history.ts
+++ packages/www/src/components/WebTerminal/history.ts
@@ -1,23 +1,13 @@
-/* eslint-disable prefer-destructuring */
-import { TerminalHistory } from './types';
-
 type Result = {
   readonly value: string;
   readonly historyPosition: number | null;
-  readonly previousHistoryPosition: number | null;
 };
 
 export default (
   direction: 'up' | 'down',
-  commandHistory: readonly TerminalHistory[],
-  historyPosition: number | null,
-  previousHistoryPosition: number | null
+  history: readonly string[],
+  historyPosition?: number | null
 ): Result | null => {
-  // Clean empty items and reverse order to ease position tracking.
-  const history = Array.from(commandHistory)
-    .filter(({ isCommand }) => isCommand)
-    .map(item => item.line)
-    .reverse();
   if (history.length === 0) {
     return null;
   }
@@ -27,43 +17,30 @@ export default (
     case 'up':
       if (historyPosition == null) {
         // If at no position, get most recent entry
-        return { value: history[0], historyPosition: 0, previousHistoryPosition: null };
+        return { value: history[0], historyPosition: 0 };
       }
       if (historyPosition + 1 === history.length) {
         // If the first entry will be reached on this press,
         // get it and decrement position by 1 to avoid confusing downscroll.
         return {
           value: history[history.length - 1],
-          historyPosition: history.length - 1,
-          previousHistoryPosition: history.length - 2
+          historyPosition: history.length - 1
         };
       }
       // Normal increment by one
       return {
         value: history[historyPosition + 1],
-        historyPosition: historyPosition + 1,
-        previousHistoryPosition: historyPosition
+        historyPosition: historyPosition + 1
       };
     case 'down':
-      if (historyPosition == null || !history[historyPosition]) {
-        // If at initial or out of range, clear (Unix-like behaviour)
-        return { value: '', historyPosition: null, previousHistoryPosition: null };
-      }
-      if (historyPosition - 1 === -1) {
-        // Clear because user pressed up once and is now pressing down again =>
-        // clear or is reaching bottom
-        const value =
-          previousHistoryPosition === null ||
-          (historyPosition === 0 && previousHistoryPosition === 1)
-            ? ''
-            : history[0];
-        return { value, historyPosition: null, previousHistoryPosition: null };
+      if (historyPosition == null || historyPosition === 0) {
+        // If at initial or will be out of range, clear (Unix-like behaviour)
+        return { value: '', historyPosition: null };
       }
       // Normal decrement by one
       return {
         value: history[historyPosition - 1],
-        historyPosition: historyPosition - 1,
-        previousHistoryPosition: historyPosition
+        historyPosition: historyPosition - 1
       };
     default:
       throw new Error();
