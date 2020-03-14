7faf8b7b22777fd9164a6861837a0861f03621e5
Sam <sam@developersam.com>
2020-03-14 18:29:36 -0400

    [samlang-demo] Better editor
    Imlpement an editor that has better scroll performance

--- packages/samlang-demo/src/LanguageDemo/InputCard.tsx
+++ packages/samlang-demo/src/LanguageDemo/InputCard.tsx
@@ -1,10 +1,10 @@
-import React, { useState, ReactElement, CSSProperties } from 'react';
+import React, { useState, ReactElement } from 'react';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-import CodeBlock from 'lib-react/CodeBlock';
 import styles from './LanguageDemo.module.css';
+import Editor from './Editor';
 
 type Props = { readonly onSubmit: (code: string) => void };
 
@@ -18,31 +18,16 @@ class Main {
 `;
 
 const rootClassName = [styles.ParallelCard, styles.EditorCard].join(' ');
-const inputClassName = [styles.EditorCardLayer, styles.EditorCardInputLayer].join(' ');
-const previewClassName = [styles.EditorCardLayer, styles.EditorCardPreviewLayer].join(' ');
 
 /**
  * The component of the language demo.
  */
 export default function InputCard({ onSubmit }: Props): ReactElement {
   const [text, setText] = useState<string>(initialText);
-  const [scrollTop, setScrollTop] = useState<number>(0);
-  const codeBlockStyle: CSSProperties = {
-    transform: `translate(0, -${scrollTop}px)`,
-    bottom: `-${scrollTop}px`
-  };
   return (
     <Card className={rootClassName}>
       <CardContent className={styles.EditorCardContainer}>
-        <textarea
-          className={inputClassName}
-          onChange={e => setText(e.currentTarget.value)}
-          onScroll={e => setScrollTop(e.currentTarget.scrollTop)}
-          value={text}
-        />
-        <CodeBlock language="samlang" className={previewClassName} style={codeBlockStyle}>
-          {text}
-        </CodeBlock>
+        <Editor code={text} onCodeChange={setText} />
       </CardContent>
       <CardActions>
         <Button size="medium" color="primary" onClick={() => onSubmit(text)}>
