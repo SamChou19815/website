554b34aca38ce1f41595422c2bf929f4d1f1d11a
Sam Zhou <sam@developersam.com>
2020-03-16 18:40:55 -0400

    [tasks] Implement all UI support for project editing and deleting (#317)

---
+++ packages/tasks/src/components/includes/ProjectCardEditForm.tsx
@@ -0,0 +1,63 @@
+import React, { ReactElement } from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import Switch from '@material-ui/core/Switch';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import TextField from '@material-ui/core/TextField';
+import { SanctionedColor } from '../../models/common-types';
+import { FormProps } from '../util/MaterialFormDialog';
+import { sanctionedColors, sanctionedColorMapping } from '../../util/constants';
+import styles from './ProjectCardEditForm.module.css';
+
+type EditableProject = {
+  readonly isPublic: boolean;
+  readonly name: string;
+  readonly color: SanctionedColor;
+};
+
+export default ({
+  values: { isPublic, name, color },
+  onChange
+}: FormProps<EditableProject>): ReactElement => (
+  <FormGroup row>
+    <FormControlLabel
+      className={styles.FormElement}
+      control={
+        // eslint-disable-next-line react/jsx-wrap-multilines
+        <Switch
+          checked={isPublic}
+          onChange={event => onChange({ isPublic: event.target.checked })}
+        />
+      }
+      label={isPublic ? 'Public' : 'Private'}
+      labelPlacement="top"
+    />
+    <TextField
+      className={styles.FormElement}
+      label="Name"
+      type="text"
+      value={name}
+      onChange={event => onChange({ name: event.currentTarget.value })}
+    />
+    <FormControl className={styles.FormElement}>
+      <InputLabel>Color</InputLabel>
+      <Select
+        value={color}
+        onChange={event => onChange({ color: event.target.value as SanctionedColor })}
+      >
+        {sanctionedColors.map(sanctionedColor => (
+          <MenuItem
+            key={sanctionedColor}
+            value={sanctionedColor}
+            style={{ color: sanctionedColorMapping[sanctionedColor] }}
+          >
+            {sanctionedColor}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  </FormGroup>
+);
--- packages/tasks/src/util/constants.ts
+++ packages/tasks/src/util/constants.ts
@@ -1,5 +1,33 @@
+import { SanctionedColor } from '../models/common-types';
+
 export const APP_NAME = 'Tasks (Alpha)';
 
 export type Page = 'Landing Page' | 'Tasks View' | 'Graph View';
 
 export const pages: readonly Page[] = ['Landing Page', 'Tasks View', 'Graph View'];
+
+export const sanctionedColors: readonly SanctionedColor[] = [
+  'Red',
+  'Pink',
+  'Purple',
+  'Indigo',
+  'Blue',
+  'Teal',
+  'Green',
+  'Light Green',
+  'Orange',
+  'Gray'
+];
+
+export const sanctionedColorMapping = {
+  Red: '#f44336',
+  Pink: '#e91e63',
+  Purple: '#9c27b0',
+  Indigo: '#3f51b5',
+  Blue: '#2196f3',
+  Teal: '#009688',
+  Green: '#4caf50',
+  'Light Green': '#8bc34a',
+  Orange: '#ff9800',
+  Gray: '#607d8b'
+} as const;
