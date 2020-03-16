ca5791bbc4847088bc219a4f9c40d27281c32970
Sam Zhou <sam@developersam.com>
2020-03-16 16:44:10 -0400

    [tasks] Allows color customization of project (#313)

--- packages/tasks/src/models/common-types.ts
+++ packages/tasks/src/models/common-types.ts
@@ -1,3 +1,15 @@
 export type NominalString<T extends string> = string & { __nominalTag__: T };
 
 export type UserEmail = NominalString<'EMAIL'>;
+
+export type SanctionedColor =
+  | 'Red'
+  | 'Pink'
+  | 'Purple'
+  | 'Indigo'
+  | 'Blue'
+  | 'Teal'
+  | 'Green'
+  | 'Light Green'
+  | 'Orange'
+  | 'Gray';
