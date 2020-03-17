54313b8b03d9755e6a2e00e26535b2a42ac9d64d
Sam <sam@developersam.com>
2020-03-17 16:40:39 -0400

    [tasks] Render task cards

--- packages/tasks/src/components/pages/ProjectDashboardPage.tsx
+++ packages/tasks/src/components/pages/ProjectDashboardPage.tsx
@@ -1,10 +1,38 @@
 import React, { ReactElement } from 'react';
+import { useSelector } from 'react-redux';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 import { RouteComponentsWithProjectIdParameter } from './router-types';
+import { ReduxStoreState, ReduxStoreTask } from '../../models/redux-store-types';
+import TaskCard from '../includes/TaskCard';
+import styles from './ProjectDashboardPage.module.css';
 
 export default ({
   match: {
     params: { projectId }
   }
-}: RouteComponentsWithProjectIdParameter): ReactElement => (
-  <div>Project {projectId} Dashboard Page</div>
-);
+}: RouteComponentsWithProjectIdParameter): ReactElement => {
+  const tasks = useSelector<ReduxStoreState, readonly ReduxStoreTask[] | null>(state =>
+    state.projects[projectId] == null
+      ? null
+      : Object.values(state.tasks).filter(task => task.projectId === projectId)
+  );
+
+  if (tasks === null) {
+    // TODO: consider the case of public project.
+    return <div>Project {projectId} does not exist in your account.</div>;
+  }
+
+  return (
+    <div>
+      <section className={styles.CardContainer}>
+        {tasks.map(task => (
+          <TaskCard key={task.taskId} task={task} />
+        ))}
+      </section>
+      <Fab color="primary" className={styles.AddTaskFab}>
+        <AddIcon />
+      </Fab>
+    </div>
+  );
+};
