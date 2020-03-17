51b42765dd0b9b6488ef9cd912e9a2cb231f1d25
Sam <sam@developersam.com>
2020-03-17 15:33:00 -0400

    [tasks] Factor out colored material card header

--- packages/tasks/src/components/includes/ProjectCard.tsx
+++ packages/tasks/src/components/includes/ProjectCard.tsx
@@ -3,14 +3,13 @@ import { useHistory } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
-import CardHeader from '@material-ui/core/CardHeader';
 import Tooltip from '@material-ui/core/Tooltip';
 import PublicIcon from '@material-ui/icons/Public';
 import AccountIcon from '@material-ui/icons/AccountCircle';
-import { SanctionedColor } from '../../models/common-types';
 import { ReduxStoreProject } from '../../models/redux-store-types';
 import ProjectCardEditForm from './ProjectCardEditForm';
 import MaterialAlertDialog from '../util/MaterialAlertDialog';
+import MaterialColoredCardHeader from '../util/MaterialColoredCardHeader';
 import MaterialFormDialog from '../util/MaterialFormDialog';
 import { editProject, deleteProject } from '../../util/firestore-actions';
 import styles from './ProjectCard.module.css';
@@ -29,42 +28,15 @@ type Props = {
   readonly project: ReduxStoreProject;
 };
 
-const getHeaderClassname = (color: SanctionedColor): string => {
-  switch (color) {
-    case 'Red':
-      return `${styles.ProjectCardHeader} ${styles.ProjectCardColorRed}`;
-    case 'Pink':
-      return `${styles.ProjectCardHeader} ${styles.ProjectCardColorPink}`;
-    case 'Purple':
-      return `${styles.ProjectCardHeader} ${styles.ProjectCardColorPurple}`;
-    case 'Indigo':
-      return `${styles.ProjectCardHeader} ${styles.ProjectCardColorIndigo}`;
-    case 'Blue':
-      return `${styles.ProjectCardHeader} ${styles.ProjectCardColorBlue}`;
-    case 'Teal':
-      return `${styles.ProjectCardHeader} ${styles.ProjectCardColorTeal}`;
-    case 'Green':
-      return `${styles.ProjectCardHeader} ${styles.ProjectCardColorGreen}`;
-    case 'Light Green':
-      return `${styles.ProjectCardHeader} ${styles.ProjectCardColorLightGreen}`;
-    case 'Orange':
-      return `${styles.ProjectCardHeader} ${styles.ProjectCardColorOrange}`;
-    case 'Gray':
-      return `${styles.ProjectCardHeader} ${styles.ProjectCardColorGray}`;
-    default:
-      throw new Error(`Unknown sanctioned color: ${color}`);
-  }
-};
 export default ({ project: { projectId, owner, isPublic, name, color } }: Props): ReactElement => {
   const routerHistory = useHistory();
 
   return (
     <Card variant="outlined" className={styles.ProjectCard}>
-      <CardHeader
-        avatar={<PublicOrPrivateIcon isPublic={isPublic} />}
-        classes={{ root: getHeaderClassname(color), title: styles.ProjectCardHeaderText }}
+      <MaterialColoredCardHeader
         title={name}
-        titleTypographyProps={{ variant: 'h4' }}
+        color={color}
+        avatar={<PublicOrPrivateIcon isPublic={isPublic} />}
         onClick={() => routerHistory.push(`/project/${projectId}`)}
       />
       <CardActions>
