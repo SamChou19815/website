dc2696df21932d0fc7413af7645bf737fb7d7843
Sam Zhou <sam@developersam.com>
2020-03-18 21:18:09 -0400

    [lib-react] Factor out pre-configured themed app (#335)

---
+++ packages/lib-react/MaterialPreConfiguredThemedApp.tsx
@@ -0,0 +1,33 @@
+import React, { ReactElement, ReactNode } from 'react';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({ palette: { primary: { main: '#3E7AE2' } } });
+
+type AppBarPosition = 'fixed' | 'absolute' | 'sticky' | 'static' | 'relative';
+
+type StyleProps = {
+  readonly app?: string;
+  readonly appBar?: string;
+  readonly title?: string;
+};
+
+type Props = {
+  readonly appBarPosition: AppBarPosition;
+  readonly styles: StyleProps;
+  readonly toolBarChildren: ReactElement;
+  readonly children: ReactNode;
+};
+
+export default ({ styles, appBarPosition, toolBarChildren, children }: Props): ReactElement => (
+  <MuiThemeProvider theme={theme}>
+    <div className={styles.app}>
+      <AppBar position={appBarPosition} className={styles.appBar}>
+        <Toolbar>{toolBarChildren}</Toolbar>
+      </AppBar>
+      {children}
+    </div>
+  </MuiThemeProvider>
+);
