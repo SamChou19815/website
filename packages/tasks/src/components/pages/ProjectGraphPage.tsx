9af60715ac06f291817e53516d769e142bbb08d8
Sam <sam@developersam.com>
2020-03-21 18:15:09 -0400

    [tasks] Draw leveled tasks without connection

--- packages/tasks/src/components/pages/ProjectGraphPage.tsx
+++ packages/tasks/src/components/pages/ProjectGraphPage.tsx
@@ -1,12 +1,9 @@
 import React, { ReactElement } from 'react';
 
 import { createProjectId } from '../../models/ids';
-import ProjectPageLayout, { TasksContainerComponentProps } from '../includes/ProjectPageLayout';
+import ProjectPageLayout from '../includes/ProjectPageLayout';
+import TaskGraphCanvas from '../includes/TaskGraphCanvas';
 import { RouteComponentsWithProjectIdParameter } from './router-types';
-
-const TaskContainer = ({ projectId }: TasksContainerComponentProps): ReactElement => {
-  return <>Graph view for project {projectId} has not been implemented yet.</>;
-};
 
 export default ({
   match: {
@@ -20,7 +17,7 @@ export default ({
         title: `Project ${project.name} Graph`,
         link: `/project/${projectId}/graph`
       })}
-      tasksContainerComponent={TaskContainer}
+      tasksContainerComponent={TaskGraphCanvas}
     />
   );
 };
