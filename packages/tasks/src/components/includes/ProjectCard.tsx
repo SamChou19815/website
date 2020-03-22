cfdb447524c7c6e0c1fef1d3d760b1472eaa2f6d
Sam <sam@developersam.com>
2020-03-22 19:58:12 -0400

    [tasks] No tooltip for project icon
    A11y is already addressed by `titleAccess`

--- packages/tasks/src/components/includes/ProjectCard.tsx
+++ packages/tasks/src/components/includes/ProjectCard.tsx
@@ -3,7 +3,6 @@ import React, { ReactElement } from 'react';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
-import Tooltip from '@material-ui/core/Tooltip';
 import AccountIcon from '@material-ui/icons/AccountCircle';
 import PublicIcon from '@material-ui/icons/Public';
 import { useHistory } from 'react-router-dom';
@@ -16,15 +15,12 @@ import MaterialFormDialog from '../util/MaterialFormDialog';
 import styles from './ProjectCard.module.css';
 import ProjectCardEditForm from './ProjectCardEditForm';
 
-const PublicOrPrivateIcon = ({ isPublic }: { readonly isPublic: boolean }): ReactElement => (
-  <Tooltip title={isPublic ? 'public' : 'private'}>
-    {isPublic ? (
-      <PublicIcon titleAccess="public" fontSize="large" />
-    ) : (
-      <AccountIcon titleAccess="private" fontSize="large" />
-    )}
-  </Tooltip>
-);
+const PublicOrPrivateIcon = ({ isPublic }: { readonly isPublic: boolean }): ReactElement =>
+  isPublic ? (
+    <PublicIcon titleAccess="public" fontSize="large" />
+  ) : (
+    <AccountIcon titleAccess="private" fontSize="large" />
+  );
 
 type Props = {
   readonly project: ReduxStoreProject;
