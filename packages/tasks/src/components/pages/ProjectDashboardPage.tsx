ee4ce4f17c1fc332dc915ca6c0bf7b1a38ac7731
Sam Zhou <sam@developersam.com>
2020-03-22 15:41:16 -0400

    [tasks] Implement masonry view for task dashboard (#366)

--- packages/tasks/src/components/pages/ProjectDashboardPage.tsx
+++ packages/tasks/src/components/pages/ProjectDashboardPage.tsx
@@ -1,8 +1,10 @@
 import React, { ReactElement, useState } from 'react';
 
 import Button from '@material-ui/core/Button';
+import Masonry from 'react-masonry-css';
 
 import { createProjectId } from '../../models/ids';
+import useWindowSize from '../hooks/useWindowSize';
 import ProjectPageLayout, { TasksContainerComponentProps } from '../includes/ProjectPageLayout';
 import TaskCard from '../includes/TaskCard';
 import TaskCardCreator from '../includes/TaskCardCreator';
@@ -12,14 +14,20 @@ import { RouteComponentsWithProjectIdParameter } from './router-types';
 const TaskContainer = ({
   projectId,
   tasks,
+  detailPanelIsOpen,
   onTaskClicked,
 }: TasksContainerComponentProps): ReactElement => {
   const [inCreationMode, setInCreationMode] = useState(false);
+
+  const breakpointColumn =
+    useWindowSize(({ width }) => {
+      const naiveComputedColumnCount = Math.floor(width / 400);
+      return Math.max(Math.min(naiveComputedColumnCount, 3), 1);
+    }) - (detailPanelIsOpen ? 1 : 0);
+
   return (
     <>
-      {inCreationMode ? (
-        <TaskCardCreator projectId={projectId} onSave={() => setInCreationMode(false)} />
-      ) : (
+      {!inCreationMode && (
         <Button
           variant="outlined"
           color="primary"
@@ -30,9 +38,28 @@ const TaskContainer = ({
           Create New Task
         </Button>
       )}
-      {tasks.map((task) => (
-        <TaskCard key={task.taskId} task={task} onHeaderClick={() => onTaskClicked(task.taskId)} />
-      ))}
+      <Masonry
+        // Force rerender when detail panel is toggled
+        breakpointCols={breakpointColumn}
+        className={styles.MasonryGrid}
+        columnClassName={styles.MasonryGridColumn}
+      >
+        {(() => {
+          const children: ReactElement[] = tasks.map((task) => (
+            <TaskCard
+              key={task.taskId}
+              task={task}
+              onHeaderClick={() => onTaskClicked(task.taskId)}
+            />
+          ));
+          if (inCreationMode) {
+            children.unshift(
+              <TaskCardCreator projectId={projectId} onSave={() => setInCreationMode(false)} />
+            );
+          }
+          return children;
+        })()}
+      </Masonry>
     </>
   );
 };
