5a1d6b2aa737201d3f01efbc47f8903e6db3e503
Sam Zhou <sam@developersam.com>
2020-03-22 18:26:14 -0400

    [lib-react][markdown] Hide `li` for task list (#369)

--- packages/lib-react/remarkable-checkbox-plugin.ts
+++ packages/lib-react/remarkable-checkbox-plugin.ts
@@ -1,52 +1,83 @@
 /* eslint-disable no-param-reassign */
 
+const SPACE = 32;
+const LBRACKET = 91;
+const RBRACKET = 93;
+const UPPER_X = 88;
+const LOWER_X = 120;
+
+interface CheckboxToken extends Remarkable.TagToken {
+  readonly checked: boolean;
+}
+
+let inputId = 0;
+const INPUT_TYPE = 'type="checkbox" disabled';
+const INPUT_CLASS = 'remarkable-task-list-item-checkbox';
+const LABEL_CLASS = 'remarkable-task-list-item-label';
+const SPAN_MARK_CLASS = 'remarkable-task-list-item-span-mark';
+
+const getNewInputId = (): string => {
+  const id = `remarkable-task-list-item-unique-id-${inputId}`;
+  inputId += 1;
+  return id;
+};
+
+const rendertasklist = (checked: boolean) => {
+  const id = getNewInputId();
+  const inputChecked = checked ? ' checked' : '';
+  const inputHTML = `<input ${INPUT_TYPE} id="${id}" class="${INPUT_CLASS}"${inputChecked} />`;
+  const spanMarkHTML = `<span class="${SPAN_MARK_CLASS}" />`;
+  const labelHTML = `<label class="${LABEL_CLASS}" for="${id}">${inputHTML}${spanMarkHTML}</label>`;
+  return labelHTML;
+};
+
 export default (md: Remarkable): void => {
   // Adapted from https://github.com/jonschlinkert/remarkable/issues/189#issuecomment-590026524
-  const rendertasklist = (checked: boolean) =>
-    checked
-      ? '<input type="checkbox" disabled class="remarkable-task-list-item-checkbox" checked />'
-      : '<input type="checkbox" disabled class="remarkable-task-list-item-checkbox" />';
 
   md.inline.ruler.push(
     'tasklist',
-    (state) => {
+    (state, silent) => {
       let { pos } = state;
       const maxpos = state.posMax;
       if (pos === maxpos) {
         return false;
       }
-      if (state.src.charCodeAt(pos) !== 91 /* [ */) {
+      if (state.src.charCodeAt(pos) !== LBRACKET) {
         return false;
       }
       pos += 1;
-      if (state.src.charCodeAt(pos) === 93 /* ] */) {
-        state.push({
-          type: 'tasklist',
-          // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-          // @ts-ignore
-          checked: false,
-          level: state.level,
-        });
+      if (state.src.charCodeAt(pos) === RBRACKET) {
+        if (!silent) {
+          state.push({
+            type: 'tasklist',
+            // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+            // @ts-ignore
+            checked: false,
+            level: state.level,
+          });
+        }
         state.pos = pos + 1;
         return true;
       }
       if (
-        state.src.charCodeAt(pos) === 120 /* x */ ||
-        state.src.charCodeAt(pos) === 88 /* X */ ||
-        state.src.charCodeAt(pos) === 32 /* Space */
+        state.src.charCodeAt(pos) === LOWER_X ||
+        state.src.charCodeAt(pos) === UPPER_X ||
+        state.src.charCodeAt(pos) === SPACE
       ) {
-        const checked = state.src.charCodeAt(pos) !== 32;
+        const checked = state.src.charCodeAt(pos) !== SPACE;
         pos += 1;
-        if (state.src.charCodeAt(pos) !== 93 /* ] */) {
+        if (state.src.charCodeAt(pos) !== RBRACKET) {
           return false;
         }
-        state.push({
-          type: 'tasklist',
-          // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-          // @ts-ignore
-          checked,
-          level: state.level,
-        });
+        if (!silent) {
+          state.push({
+            type: 'tasklist',
+            // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+            // @ts-ignore
+            checked,
+            level: state.level,
+          });
+        }
         state.pos = pos + 1;
         return true;
       }
@@ -54,9 +85,9 @@ export default (md: Remarkable): void => {
     },
     {}
   );
-  md.renderer.rules.tasklist = (tokens, index) => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-    // @ts-ignore
-    return rendertasklist(tokens[index].checked);
-  };
+
+  md.renderer.rules.tasklist = (tokens, index) =>
+    rendertasklist((tokens[index] as CheckboxToken).checked);
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  md.renderer.rules.list_item_open = () => '<li class="remarkable-li">';
 };
