3a9d695686569adcb9efa3f5ed8a48b1dd5f7e00
Sam Zhou <sam@developersam.com>
2020-03-21 23:44:16 -0400

    [tasks] Split graph components computation and drawing (#359)
    To prepare migrating to SVG based graph

--- packages/tasks/src/components/includes/TaskGraphCanvas.graph.ts
+++ packages/tasks/src/components/includes/TaskGraphCanvas.graph.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-param-reassign */
 import solver, { IModel } from 'javascript-lp-solver';
 
+import { TaskId } from '../../models/ids';
 import { leveledTopologicalSort } from '../../models/redux-store-task';
 import { ReduxStoreTask } from '../../models/redux-store-types';
 
@@ -14,9 +15,7 @@ import { ReduxStoreTask } from '../../models/redux-store-types';
  * @returns a list of ordered tasks, each inner list is an ordered list of tasks.
  * Task should appear in this order during rendering.
  */
-export default function minimizeCross(
-  tasks: readonly ReduxStoreTask[]
-): readonly ReduxStoreTask[][] {
+export const minimizeCross = (tasks: readonly ReduxStoreTask[]): readonly ReduxStoreTask[][] => {
   const taskGraph: { readonly [key: string]: ReduxStoreTask } = (() => {
     const mutableTaskMap: { [key: string]: ReduxStoreTask } = {};
     tasks.forEach((task) => {
@@ -139,4 +138,130 @@ export default function minimizeCross(
   }
 
   return decrossOpt(leveledTopologicalSort(tasks) as readonly ReduxStoreTask[][]);
-}
+};
+
+type TaskRectangle = { readonly startX: number; readonly startY: number };
+type GraphCardComponent = TaskRectangle & {
+  readonly width: number;
+  readonly height: number;
+  readonly color: string;
+};
+type GraphTextComponent = TaskRectangle & { readonly text: string; readonly maxWidth: number };
+type GraphArrowComponent = TaskRectangle & {
+  readonly endX: number;
+  readonly endY: number;
+  readonly controlX: number;
+  readonly controlY: number;
+};
+type GraphComponents = {
+  readonly cards: readonly GraphCardComponent[];
+  readonly texts: readonly GraphTextComponent[];
+  readonly arrows: readonly GraphArrowComponent[];
+  readonly canvasWidth: number;
+  readonly canvasHeight: number;
+  readonly textSize: number;
+};
+
+const MARGIN = 64;
+const PADDING = 16;
+const TEXT_SIZE = 20;
+const WIDTH = 300;
+const HEIGHT = 64;
+const MAX_UNTRIMMED_TEXT_LENGTH = 27;
+
+const getTaskRectangle = (level: number, index: number): TaskRectangle => ({
+  startX: index * (MARGIN + WIDTH) + MARGIN / 2,
+  startY: level * (MARGIN + HEIGHT) + MARGIN / 2,
+});
+const autoTrimText = (text: string): string => {
+  if (text.length < MAX_UNTRIMMED_TEXT_LENGTH) {
+    return text;
+  }
+  return `${text.substring(0, MAX_UNTRIMMED_TEXT_LENGTH)}...`;
+};
+
+export const generateGraphComponents = (
+  taskWithPositionMap: { readonly [taskId: string]: Readonly<{ level: number; index: number }> },
+  leveledTasks: readonly (readonly ReduxStoreTask[])[],
+  colors: { readonly [projectId: string]: string }
+): GraphComponents => {
+  const cards: GraphCardComponent[] = [];
+  const texts: GraphTextComponent[] = [];
+  const arrows: GraphArrowComponent[] = [];
+  const canvasWidth =
+    (MARGIN + WIDTH) *
+    leveledTasks.reduce((max, tasksInLevel) => Math.max(max, tasksInLevel.length), 0);
+  const canvasHeight = (MARGIN + HEIGHT) * leveledTasks.length;
+
+  // Draw Task Cards
+  leveledTasks.forEach((tasksInLevel, level) => {
+    tasksInLevel.forEach((task, index) => {
+      const { startX, startY } = getTaskRectangle(level, index);
+      cards.push({
+        startX,
+        startY,
+        width: WIDTH,
+        height: HEIGHT,
+        color: colors[task.projectId],
+      });
+
+      texts.push({
+        text: autoTrimText(task.name),
+        startX: startX + PADDING / 2,
+        startY: startY + TEXT_SIZE / 2 + HEIGHT / 2,
+        maxWidth: WIDTH - PADDING,
+      });
+    });
+  });
+
+  // Draw Dependency Arrows
+  leveledTasks.forEach((tasksInLevel, level) => {
+    tasksInLevel.forEach((task, index) => {
+      const { startX: endX, startY: endY } = getTaskRectangle(level, index);
+      task.dependencies.forEach((dependentTaskId) => {
+        const dependentTaskWithPosition = taskWithPositionMap[dependentTaskId];
+        if (dependentTaskWithPosition == null) {
+          return;
+        }
+        const { startX, startY } = getTaskRectangle(
+          dependentTaskWithPosition.level,
+          dependentTaskWithPosition.index
+        );
+        arrows.push({
+          startX: startX + WIDTH / 2,
+          startY: startY + HEIGHT,
+          endX: endX + WIDTH / 2,
+          endY,
+          controlX: endX + WIDTH / 2 - ((endX - startX) * 1) / 3,
+          controlY: endY,
+        });
+      });
+    });
+  });
+
+  return { cards, texts, arrows, canvasWidth, canvasHeight, textSize: TEXT_SIZE };
+};
+
+export const clickTask = (
+  leveledTasks: readonly (readonly ReduxStoreTask[])[],
+  relativeX: number,
+  relativeY: number,
+  onTaskClicked: (id: TaskId) => void
+): void => {
+  leveledTasks.forEach((tasksInLevel, level) => {
+    tasksInLevel.forEach((task, index) => {
+      if (task === null) {
+        return;
+      }
+      const rectangle = getTaskRectangle(level, index);
+      if (
+        relativeX >= rectangle.startX &&
+        relativeX <= rectangle.startX + WIDTH &&
+        relativeY >= rectangle.startY &&
+        relativeY <= rectangle.startY + HEIGHT
+      ) {
+        onTaskClicked(task.taskId);
+      }
+    });
+  });
+};
--- packages/tasks/src/components/includes/TaskGraphCanvas.tsx
+++ packages/tasks/src/components/includes/TaskGraphCanvas.tsx
@@ -2,37 +2,11 @@ import React, { ReactElement, MouseEvent, useRef, useEffect } from 'react';
 
 import { useSelector } from 'react-redux';
 
-import { ReduxStoreState, ReduxStoreTask } from '../../models/redux-store-types';
+import { ReduxStoreState } from '../../models/redux-store-types';
 import { sanctionedColorMapping } from '../../util/constants';
 import { TasksContainerComponentProps } from './ProjectPageLayout';
-import minimizeCross from './TaskGraphCanvas.graph';
+import { minimizeCross, generateGraphComponents, clickTask } from './TaskGraphCanvas.graph';
 import styles from './TaskGraphCanvas.module.css';
-
-const MARGIN = 64;
-const PADDING = 16;
-const TEXT_SIZE = 20;
-const WIDTH = 300;
-const HEIGHT = 64;
-
-const MAX_UNTRIMMED_TEXT_LENGTH = 27;
-
-type TaskRectangle = { readonly startX: number; readonly startY: number };
-
-type TaskGrid = readonly (readonly (ReduxStoreTask | null)[])[];
-
-const createTaskGrid = (tasks: readonly ReduxStoreTask[]): TaskGrid => minimizeCross(tasks);
-
-const getTaskRectangle = (level: number, index: number): TaskRectangle => ({
-  startX: index * (MARGIN + WIDTH) + MARGIN / 2,
-  startY: level * (MARGIN + HEIGHT) + MARGIN / 2,
-});
-
-const autoTrimText = (text: string): string => {
-  if (text.length < MAX_UNTRIMMED_TEXT_LENGTH) {
-    return text;
-  }
-  return `${text.substring(0, MAX_UNTRIMMED_TEXT_LENGTH)}...`;
-};
 
 export default ({ tasks, onTaskClicked }: TasksContainerComponentProps): ReactElement => {
   const colors = useSelector((state: ReduxStoreState) => {
@@ -44,83 +18,20 @@ export default ({ tasks, onTaskClicked }: TasksContainerComponentProps): ReactEl
   });
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const canvasRef = useRef<HTMLCanvasElement>(undefined!);
-  const leveledTasks = createTaskGrid(tasks);
+  const leveledTasks = minimizeCross(tasks);
 
-  const canvasWidth =
-    (MARGIN + WIDTH) *
-    leveledTasks.reduce((max, tasksInLevel) => Math.max(max, tasksInLevel.length), 0);
-  const canvasHeight = (MARGIN + HEIGHT) * leveledTasks.length;
-
-  useEffect(() => {
-    const canvasContext = canvasRef.current.getContext('2d');
-    if (canvasContext == null) {
-      throw new Error('Invalid canvas context!');
-    }
-
-    type TaskWithPositionMap = { [taskId: string]: Readonly<{ level: number; index: number }> };
-    const taskWithPositionMap: TaskWithPositionMap = {};
-    leveledTasks.forEach((tasksInLevel, level) => {
-      tasksInLevel.forEach((task, index) => {
-        if (task === null) {
-          return;
-        }
-        taskWithPositionMap[task.taskId] = { level, index };
-      });
+  type TaskWithPositionMap = { [taskId: string]: Readonly<{ level: number; index: number }> };
+  const taskWithPositionMap: TaskWithPositionMap = {};
+  leveledTasks.forEach((tasksInLevel, level) => {
+    tasksInLevel.forEach((task, index) => {
+      if (task === null) {
+        return;
+      }
+      taskWithPositionMap[task.taskId] = { level, index };
     });
+  });
 
-    canvasContext.clearRect(0, 0, canvasWidth, canvasHeight);
-
-    canvasContext.font = `${TEXT_SIZE}px sans-serif`;
-
-    // Draw Task Cards
-    leveledTasks.forEach((tasksInLevel, level) => {
-      tasksInLevel.forEach((task, index) => {
-        if (task === null) {
-          return;
-        }
-        const { startX, startY } = getTaskRectangle(level, index);
-        const color = colors[task.projectId];
-        canvasContext.fillStyle = color;
-        canvasContext.fillRect(startX, startY, WIDTH, HEIGHT);
-        canvasContext.fillStyle = 'white';
-        canvasContext.fillText(
-          autoTrimText(task.name),
-          startX + PADDING / 2,
-          startY + TEXT_SIZE / 2 + HEIGHT / 2,
-          WIDTH - PADDING
-        );
-      });
-    });
-
-    // Draw Dependency Arrows
-    leveledTasks.forEach((tasksInLevel, level) => {
-      tasksInLevel.forEach((task, index) => {
-        if (task === null) {
-          return;
-        }
-        const { startX: endX, startY: endY } = getTaskRectangle(level, index);
-        task.dependencies.forEach((dependentTaskId) => {
-          const dependentTaskWithPosition = taskWithPositionMap[dependentTaskId];
-          if (dependentTaskWithPosition == null) {
-            return;
-          }
-          const { startX, startY } = getTaskRectangle(
-            dependentTaskWithPosition.level,
-            dependentTaskWithPosition.index
-          );
-          canvasContext.beginPath();
-          canvasContext.moveTo(startX + WIDTH / 2, startY + HEIGHT);
-          canvasContext.quadraticCurveTo(
-            endX + WIDTH / 2 - ((endX - startX) * 1) / 3,
-            endY,
-            endX + WIDTH / 2,
-            endY
-          );
-          canvasContext.stroke();
-        });
-      });
-    });
-  }, [colors, leveledTasks, canvasWidth, canvasHeight]);
+  const graphComponents = generateGraphComponents(taskWithPositionMap, leveledTasks, colors);
 
   const onCanvasClick = (event: MouseEvent<HTMLCanvasElement>): void => {
     const { clientX, clientY } = event;
@@ -130,32 +41,49 @@ export default ({ tasks, onTaskClicked }: TasksContainerComponentProps): ReactEl
     const relativeY =
       clientY + document.body.scrollTop + document.documentElement.scrollTop - canvas.offsetTop;
 
-    leveledTasks.forEach((tasksInLevel, level) => {
-      tasksInLevel.forEach((task, index) => {
-        if (task === null) {
-          return;
-        }
-        const rectangle = getTaskRectangle(level, index);
-        if (
-          relativeX >= rectangle.startX &&
-          relativeX <= rectangle.startX + WIDTH &&
-          relativeY >= rectangle.startY &&
-          relativeY <= rectangle.startY + HEIGHT
-        ) {
-          onTaskClicked(task.taskId);
-        }
-      });
-    });
+    clickTask(leveledTasks, relativeX, relativeY, onTaskClicked);
   };
+
+  useEffect(() => {
+    const canvasContext = canvasRef.current.getContext('2d');
+    if (canvasContext == null) {
+      throw new Error('Invalid canvas context!');
+    }
+
+    const { cards, texts, arrows, canvasWidth, canvasHeight, textSize } = graphComponents;
+
+    canvasContext.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    // Draw Cards
+    cards.forEach(({ startX, startY, width, height, color }) => {
+      canvasContext.fillStyle = color;
+      canvasContext.fillRect(startX, startY, width, height);
+    });
+
+    // Draw Text
+    canvasContext.font = `${textSize}px sans-serif`;
+    canvasContext.fillStyle = 'white';
+    texts.forEach(({ text, startX, startY, maxWidth }) => {
+      canvasContext.fillText(text, startX, startY, maxWidth);
+    });
+
+    // Draw Dependency Arrows
+    arrows.forEach(({ startX, startY, endX, endY, controlX, controlY }) => {
+      canvasContext.beginPath();
+      canvasContext.moveTo(startX, startY);
+      canvasContext.quadraticCurveTo(controlX, controlY, endX, endY);
+      canvasContext.stroke();
+    });
+  }, [graphComponents]);
 
   return (
     <canvas
       ref={canvasRef}
       className={styles.Canvas}
       onClick={onCanvasClick}
-      width={`${canvasWidth}px`}
-      height={`${canvasHeight}px`}
-      style={{ width: `${canvasWidth}px` }}
+      width={`${graphComponents.canvasWidth}px`}
+      height={`${graphComponents.canvasHeight}px`}
+      style={{ width: `${graphComponents.canvasWidth}px` }}
     />
   );
 };
