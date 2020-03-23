5ba8baa0e010431de1b431494c211682c5f5b914
Sam <sam@developersam.com>
2020-03-22 20:07:47 -0400

    [tasks] Introduce masonry layout in tasks panel

--- packages/tasks/src/components/includes/TasksPanel.tsx
+++ packages/tasks/src/components/includes/TasksPanel.tsx
@@ -1,10 +1,12 @@
 import React, { ReactElement, useState } from 'react';
 
+import Masonry from 'react-masonry-css';
 import { useSelector } from 'react-redux';
 
 import { TaskId } from '../../models/ids';
 import { flattenedTopologicalSort } from '../../models/redux-store-task';
 import { ReduxStoreState, ReduxStoreTask } from '../../models/redux-store-types';
+import useWindowSize from '../hooks/useWindowSize';
 import TaskCard from './TaskCard';
 import TaskDetailPanel from './TaskDetailPanel';
 import styles from './TasksPanel.module.css';
@@ -15,18 +17,30 @@ export default ({ className }: { readonly className?: string }): ReactElement =>
   );
   const [taskDetailPanelTaskId, setTaskDetailPanelTaskId] = useState<TaskId | null>(null);
 
+  const breakpointColumn =
+    useWindowSize(({ width }) => {
+      const naiveComputedColumnCount = Math.floor(width / 400);
+      return Math.max(Math.min(naiveComputedColumnCount, 3), 1);
+    }) - (taskDetailPanelTaskId !== null ? 2 : 1);
+
   return (
     <div className={className}>
       <section
         className={taskDetailPanelTaskId === null ? undefined : styles.MainTasksContainerSquezzed}
       >
-        {tasks.map((task) => (
-          <TaskCard
-            key={task.taskId}
-            task={task}
-            onHeaderClick={() => setTaskDetailPanelTaskId(task.taskId)}
-          />
-        ))}
+        <Masonry
+          breakpointCols={breakpointColumn}
+          className="masonry-grid"
+          columnClassName="masonry-grid-column"
+        >
+          {tasks.map((task) => (
+            <TaskCard
+              key={task.taskId}
+              task={task}
+              onHeaderClick={() => setTaskDetailPanelTaskId(task.taskId)}
+            />
+          ))}
+        </Masonry>
       </section>
       {taskDetailPanelTaskId && (
         <TaskDetailPanel
