3fe3811386992ccc6a6dc3286b9e2ff2fb95c83f
Sam <sam@developersam.com>
2020-03-22 22:30:05 -0400

    [tasks] Unify project dashboard and graph view into one page

--- packages/tasks/src/components/pages/RootRouter.tsx
+++ packages/tasks/src/components/pages/RootRouter.tsx
@@ -3,13 +3,11 @@ import React, { ReactElement } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './HomePage';
-import ProjectDashboardPage from './ProjectDashboardPage';
-import ProjectGraphPage from './ProjectGraphPage';
+import ProjectPage from './ProjectPage';
 
 export default (): ReactElement => (
   <Switch>
-    <Route path="/project/:projectId/graph" exact component={ProjectGraphPage} />
-    <Route path="/project/:projectId" exact component={ProjectDashboardPage} />
+    <Route path="/project/:projectId" exact component={ProjectPage} />
     <Route path="/" exact component={HomePage} />
   </Switch>
 );
