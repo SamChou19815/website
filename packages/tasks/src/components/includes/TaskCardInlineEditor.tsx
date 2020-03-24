5c34199c57eec60cff8da1be84cdb0426c9ba085
Sam <sam@developersam.com>
2020-03-24 16:57:12 -0400

    [tasks] Factor out `TaskEditorForm`

--- packages/tasks/src/components/includes/TaskCardInlineEditor.tsx
+++ packages/tasks/src/components/includes/TaskCardInlineEditor.tsx
@@ -1,26 +1,12 @@
-import React, { ReactElement, ChangeEvent } from 'react';
+import React, { ReactElement } from 'react';
 
 import Button from '@material-ui/core/Button';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import FormGroup from '@material-ui/core/FormGroup';
-import TextField from '@material-ui/core/TextField';
-import Autocomplete from '@material-ui/lab/Autocomplete';
-import { useSelector } from 'react-redux';
 
 import { TaskId, ProjectId } from '../../models/ids';
-import { ReduxStoreTask, ReduxStoreState, ReduxStoreProject } from '../../models/redux-store-types';
-import { sanctionedColorMapping } from '../../util/constants';
 import useFormManager from '../hooks/useFormManager';
-import { useEligibleDependencies } from '../hooks/useTasks';
-import styles from './TaskCardInlineEditor.module.css';
-
-type EditableTask = {
-  readonly projectId: ProjectId | undefined;
-  readonly name: string;
-  readonly content: string;
-  readonly dependencies: readonly TaskId[];
-};
+import TaskEditorForm, { EditableTask } from './TaskEditorForm';
 
 type ValidatedEditableTask = Omit<EditableTask, 'projectId'> & { readonly projectId: ProjectId };
 
@@ -40,97 +26,16 @@ export default ({
   onSave,
 }: Props): ReactElement => {
   const [editableTask, setPartialEditableTask] = useFormManager(initialEditableTask);
-  const { projectId, name, content, dependencies } = editableTask;
-  const projects = useSelector((state: ReduxStoreState) => state.projects);
-  const [allDependenciesTaskOptions, eligibleOptions] = useEligibleDependencies(
-    taskId,
-    projectId,
-    dependencies
-  ) as readonly [ReduxStoreTask[], ReduxStoreTask[]];
-
-  const projectList = Object.values(projects);
 
   return (
     <>
       <CardContent>
-        <FormGroup row={false}>
-          <Autocomplete
-            disabled={initialProjectId !== undefined}
-            className={styles.FormElement}
-            options={projectList}
-            autoHighlight
-            getOptionLabel={(option) => option.name}
-            renderOption={(option) => {
-              const color = sanctionedColorMapping[projects[option.projectId].color];
-              return (
-                <>
-                  <span className={styles.ColorDot} style={{ backgroundColor: color }} />
-                  <span>{option.name}</span>
-                </>
-              );
-            }}
-            value={projectId === undefined ? null : projects[projectId]}
-            onChange={(_: ChangeEvent<{}>, value: ReduxStoreProject | null) => {
-              setPartialEditableTask({ projectId: value == null ? undefined : value.projectId });
-            }}
-            renderInput={(params) => (
-              // eslint-disable-next-line react/jsx-props-no-spreading
-              <TextField label="Project" type="text" {...params} />
-            )}
-          />
-          <TextField
-            className={styles.FormElement}
-            label="Name"
-            type="text"
-            value={name}
-            onChange={(event) => setPartialEditableTask({ name: event.currentTarget.value })}
-          />
-          <TextField
-            className={styles.FormElement}
-            label="Content"
-            type="text"
-            value={content}
-            onChange={(event) => setPartialEditableTask({ content: event.currentTarget.value })}
-            multiline
-          />
-          <Autocomplete
-            multiple
-            key={projectId || 'none'}
-            disabled={projectId === undefined}
-            className={styles.FormElement}
-            options={eligibleOptions}
-            autoHighlight
-            getOptionLabel={(option) => option.name}
-            renderOption={(option) => {
-              const color = sanctionedColorMapping[projects[option.projectId].color];
-              return (
-                <>
-                  <span className={styles.ColorDot} style={{ backgroundColor: color }} />
-                  <span>{option.name}</span>
-                </>
-              );
-            }}
-            value={(() => {
-              const tasks: ReduxStoreTask[] = [];
-              for (let i = 0; i < dependencies.length; i += 1) {
-                const id = dependencies[i];
-                const task = allDependenciesTaskOptions.find((oneTask) => oneTask.taskId === id);
-                if (task == null) {
-                  return [];
-                }
-                tasks.push(task);
-              }
-              return tasks;
-            })()}
-            onChange={(_, values) => {
-              setPartialEditableTask({ dependencies: values.map((task) => task.taskId) });
-            }}
-            renderInput={(params) => (
-              // eslint-disable-next-line react/jsx-props-no-spreading
-              <TextField label="Dependencies" type="text" {...params} />
-            )}
-          />
-        </FormGroup>
+        <TaskEditorForm
+          taskId={taskId}
+          initialProjectId={initialProjectId}
+          editableTask={editableTask}
+          onEdit={setPartialEditableTask}
+        />
       </CardContent>
       <CardActions>
         <Button size="small" color="primary" onClick={onDiscard}>
@@ -139,7 +44,7 @@ export default ({
         <Button
           size="small"
           color="primary"
-          disabled={name.trim().length === 0 || editableTask.projectId === undefined}
+          disabled={editableTask.name.trim().length === 0 || editableTask.projectId === undefined}
           onClick={() => {
             const { projectId: id } = editableTask;
             if (id === undefined) {
---
+++ packages/tasks/src/components/includes/TaskEditorForm.tsx
@@ -0,0 +1,117 @@
+import React, { ReactElement, ChangeEvent } from 'react';
+
+import FormGroup from '@material-ui/core/FormGroup';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { useSelector } from 'react-redux';
+
+import { TaskId, ProjectId } from '../../models/ids';
+import { ReduxStoreTask, ReduxStoreState, ReduxStoreProject } from '../../models/redux-store-types';
+import { sanctionedColorMapping } from '../../util/constants';
+import { useEligibleDependencies } from '../hooks/useTasks';
+import styles from './TaskCardInlineEditor.module.css';
+
+export type EditableTask = {
+  readonly projectId: ProjectId | undefined;
+  readonly name: string;
+  readonly content: string;
+  readonly dependencies: readonly TaskId[];
+};
+
+type Props = {
+  readonly taskId: TaskId | null;
+  readonly initialProjectId?: ProjectId;
+  readonly editableTask: EditableTask;
+  readonly onEdit: (change: Partial<EditableTask>) => void;
+};
+
+export default ({ taskId, initialProjectId, editableTask, onEdit }: Props): ReactElement => {
+  const { projectId, name, content, dependencies } = editableTask;
+  const projects = useSelector((state: ReduxStoreState) => state.projects);
+  const [allDependenciesTaskOptions, eligibleOptions] = useEligibleDependencies(
+    taskId,
+    projectId,
+    dependencies
+  ) as readonly [ReduxStoreTask[], ReduxStoreTask[]];
+
+  const projectList = Object.values(projects);
+
+  return (
+    <FormGroup row={false}>
+      <Autocomplete
+        disabled={initialProjectId !== undefined}
+        className={styles.FormElement}
+        options={projectList}
+        autoHighlight
+        getOptionLabel={(option) => option.name}
+        renderOption={(option) => {
+          const color = sanctionedColorMapping[projects[option.projectId].color];
+          return (
+            <>
+              <span className={styles.ColorDot} style={{ backgroundColor: color }} />
+              <span>{option.name}</span>
+            </>
+          );
+        }}
+        value={projectId === undefined ? null : projects[projectId]}
+        onChange={(_: ChangeEvent<{}>, value: ReduxStoreProject | null) => {
+          onEdit({ projectId: value == null ? undefined : value.projectId });
+        }}
+        renderInput={(params) => (
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          <TextField label="Project" type="text" {...params} />
+        )}
+      />
+      <TextField
+        className={styles.FormElement}
+        label="Name"
+        type="text"
+        value={name}
+        onChange={(event) => onEdit({ name: event.currentTarget.value })}
+      />
+      <TextField
+        className={styles.FormElement}
+        label="Content"
+        type="text"
+        value={content}
+        onChange={(event) => onEdit({ content: event.currentTarget.value })}
+        multiline
+      />
+      <Autocomplete
+        multiple
+        key={projectId || 'none'}
+        disabled={projectId === undefined}
+        className={styles.FormElement}
+        options={eligibleOptions}
+        autoHighlight
+        getOptionLabel={(option) => option.name}
+        renderOption={(option) => {
+          const color = sanctionedColorMapping[projects[option.projectId].color];
+          return (
+            <>
+              <span className={styles.ColorDot} style={{ backgroundColor: color }} />
+              <span>{option.name}</span>
+            </>
+          );
+        }}
+        value={(() => {
+          const tasks: ReduxStoreTask[] = [];
+          for (let i = 0; i < dependencies.length; i += 1) {
+            const id = dependencies[i];
+            const task = allDependenciesTaskOptions.find((oneTask) => oneTask.taskId === id);
+            if (task == null) {
+              return [];
+            }
+            tasks.push(task);
+          }
+          return tasks;
+        })()}
+        onChange={(_, values) => onEdit({ dependencies: values.map((task) => task.taskId) })}
+        renderInput={(params) => (
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          <TextField label="Dependencies" type="text" {...params} />
+        )}
+      />
+    </FormGroup>
+  );
+};
