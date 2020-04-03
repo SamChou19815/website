1c8b9ebcbcd0f7a530b74fdf8dff66fadc27c8d1
Sam <sam@developersam.com>
2020-04-03 19:08:44 -0400

    [queue] Add basic styling to the queue app with material design

--- packages/queue/src/components/MainApp.tsx
+++ packages/queue/src/components/MainApp.tsx
@@ -1,6 +1,15 @@
 import React, { ReactElement, useState } from 'react';
 
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+
 import { AppQueue } from '../models/types';
+import { queuesCollection } from '../util/firestore';
 import { createNewQueue } from '../util/firestore-actions';
 import { useQueues } from '../util/use-collections';
 import LoadingPage from './LoadingPage';
@@ -16,54 +25,82 @@ export default (): ReactElement => {
   }
 
   return (
-    <div>
+    <div className="card-container">
       {currentQueue === null && (
-        <div>
-          <h2>Queues you own:</h2>
+        <>
+          <Typography component="h2" variant="h4" className="centered-title">
+            Owned Queues
+          </Typography>
+          {queues.length === 0 && <div>No owned queues yet.</div>}
           {queues.map((queue) => (
-            <div key={queue.queueId}>
-              <div>{queue.name}</div>
-              <button type="button" onClick={() => setCurrentQueue(queue)}>
-                Manage
-              </button>
-            </div>
+            <Card key={queue.queueId} variant="outlined" className="common-card">
+              <CardHeader title={queue.name} />
+              <CardActions>
+                <Button size="small" color="primary" onClick={() => setCurrentQueue(queue)}>
+                  Manage
+                </Button>
+                <Button
+                  size="small"
+                  color="primary"
+                  onClick={() => queuesCollection.doc(queue.queueId).delete()}
+                >
+                  Delete
+                </Button>
+              </CardActions>
+            </Card>
           ))}
-        </div>
+        </>
       )}
       {currentQueue === null && newQueueName !== null && (
-        <div>
-          <h2>Queue Creator</h2>
-          <div>
-            <input
+        <Card variant="outlined" className="common-card">
+          <CardHeader title="Queue Creator" />
+          <CardContent>
+            <TextField
+              label="Queue name"
               type="text"
-              placeholder="Queue name"
+              className="text-input"
               value={newQueueName}
               onChange={(event) => setNewQueueName(event.currentTarget.value)}
             />
-          </div>
-          <button
-            type="button"
-            onClick={() => {
-              if (newQueueName === null) {
-                throw new Error();
-              }
-              createNewQueue(newQueueName);
-              setNewQueueName(null);
-            }}
-          >
-            Submit
-          </button>
-        </div>
+          </CardContent>
+          <CardActions>
+            <Button
+              size="small"
+              color="primary"
+              onClick={() => {
+                if (newQueueName === null) {
+                  throw new Error();
+                }
+                createNewQueue(newQueueName);
+                setNewQueueName(null);
+              }}
+            >
+              Submit
+            </Button>
+          </CardActions>
+        </Card>
       )}
       {currentQueue === null && newQueueName === null && (
-        <button type="button" onClick={() => setNewQueueName('')}>
+        <Button
+          variant="outlined"
+          color="primary"
+          className="centered-button"
+          onClick={() => setNewQueueName('')}
+          disableElevation
+        >
           Create new queue
-        </button>
+        </Button>
       )}
       {currentQueue !== null && (
-        <button type="button" onClick={() => setCurrentQueue(null)}>
+        <Button
+          variant="outlined"
+          color="primary"
+          className="centered-button"
+          onClick={() => setCurrentQueue(null)}
+          disableElevation
+        >
           Back to queues
-        </button>
+        </Button>
       )}
       {currentQueue !== null && <QueueView queue={currentQueue} />}
     </div>
