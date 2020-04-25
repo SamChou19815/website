ea76417ce0b48f04314f298efa20880f2e6dcc23
Sam Zhou <sam@developersam.com>
2020-04-25 19:49:56 -0400

    [tasks] Remove all routing logic (#453)

--- packages/tasks/src/components/util/MaterialThemedNavigableAppContainer.tsx
+++ packages/tasks/src/components/util/MaterialThemedNavigableAppContainer.tsx
@@ -3,43 +3,23 @@ import React, { ReactElement, ReactNode } from 'react';
 import { Typography } from '@material-ui/core';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import MaterialPreConfiguredThemedApp from 'lib-react/MaterialPreConfiguredThemedApp';
-import { useHistory } from 'react-router';
 
 import { APP_NAME } from '../../util/constants';
 import styles from './MaterialThemedNavigableAppContainer.module.css';
 
-export type NestedNavigationLevel = { readonly title: string; readonly link?: string };
+export type NestedNavigationLevel = { readonly title: string };
 
 type Props = {
-  readonly nestedNavigationLevels?: readonly NestedNavigationLevel[];
   readonly buttons?: ReactElement;
   readonly children: ReactNode;
 };
 
-export default ({ nestedNavigationLevels = [], buttons, children }: Props): ReactElement => {
-  const history = useHistory();
-
+export default ({ buttons, children }: Props): ReactElement => {
   const toolBarChildren = (
     <Breadcrumbs aria-label="breadcrumb" className={styles.Title}>
-      <Typography
-        variant="h6"
-        color="inherit"
-        onClick={() => history.push('/')}
-        className={styles.TitleLink}
-      >
+      <Typography variant="h6" color="inherit" className={styles.TitleLink}>
         {APP_NAME}
       </Typography>
-      {nestedNavigationLevels.map(({ title, link }) => (
-        <Typography
-          key={title}
-          variant="h6"
-          color="inherit"
-          onClick={link === undefined ? undefined : () => history.push(link)}
-          className={styles.TitleLink}
-        >
-          {title}
-        </Typography>
-      ))}
     </Breadcrumbs>
   );
 
