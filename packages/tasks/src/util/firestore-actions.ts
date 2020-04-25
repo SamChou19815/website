d355e58f7eb5a0576bf241c45b175d0cb5605418
Sam Zhou <sam@developersam.com>
2020-04-25 19:57:11 -0400

    [tasks] Integrate with firebase analytics (#454)

--- packages/tasks/src/util/firestore-actions.ts
+++ packages/tasks/src/util/firestore-actions.ts
@@ -2,16 +2,20 @@ import { fromNewReduxStoreTask, fromPartialReduxStoreTask } from '../models/fire
 import { FirestoreTask } from '../models/firestore-types';
 import { TaskId } from '../models/ids';
 import { ReduxStoreTask } from '../models/redux-store-types';
+import { reportAddTaskEvent, reportEditTaskEvent, reportDeleteTaskEvent } from './analytics';
 import { tasksCollection } from './firestore';
 
 export const createTask = (task: Omit<ReduxStoreTask, 'taskId'>): void => {
-  tasksCollection.add(fromNewReduxStoreTask(task));
+  tasksCollection.add(fromNewReduxStoreTask(task)).then(reportAddTaskEvent);
 };
 
 export const editTask = (task: Partial<FirestoreTask> & { readonly taskId: TaskId }): void => {
-  tasksCollection.doc(task.taskId).update(fromPartialReduxStoreTask(task));
+  tasksCollection
+    .doc(task.taskId)
+    .update(fromPartialReduxStoreTask(task))
+    .then(reportEditTaskEvent);
 };
 
 export const deleteTask = (taskId: string): void => {
-  tasksCollection.doc(taskId).delete();
+  tasksCollection.doc(taskId).delete().then(reportDeleteTaskEvent);
 };
