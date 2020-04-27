ba628202f0df1736c569283787cbf5e72ad1886e
Sam <sam@developersam.com>
2020-04-27 13:23:35 -0400

    Make types reference react-scripts types as well

--- packages/blog/types.d.ts
+++ packages/blog/types.d.ts
@@ -1,2 +1,4 @@
 // eslint-disable-next-line spaced-comment
 /// <reference types="@docusaurus/module-type-aliases" />
+// eslint-disable-next-line spaced-comment
+/// <reference types="react-scripts" />
