f7c912b4464c3141ecebf0cbedaa935891e19060
Sam <sam@developersam.com>
2020-04-27 21:14:20 -0400

    [tasks] Better icons for each status

--- packages/tasks/src/components/includes/CheckboxIcon.tsx
+++ packages/tasks/src/components/includes/CheckboxIcon.tsx
@@ -1,18 +1,21 @@
+import Archive from '@material-ui/icons/Archive';
+import Build from '@material-ui/icons/Build';
 import CheckBox from '@material-ui/icons/CheckBox';
 import CheckBoxOutlineBlank from '@material-ui/icons/CheckBoxOutlineBlank';
-import IndeterminateCheckBox from '@material-ui/icons/IndeterminateCheckBox';
 
 import { TaskStatus } from '../../models/common-types';
 
 export default (status: TaskStatus): typeof CheckBox => {
   switch (status) {
+    case 'backlogged':
+      return Archive;
     case 'to-do':
       return CheckBoxOutlineBlank;
     case 'in-progress':
-      return IndeterminateCheckBox;
+      return Build;
     case 'done':
       return CheckBox;
     default:
-      throw new Error();
+      throw new Error(status);
   }
 };
