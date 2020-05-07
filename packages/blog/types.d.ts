ce2b0ff4731e469a362e911781b734e3df99a43f
Sam <sam@developersam.com>
2020-05-07 18:53:01 -0400

    [docusaurus] Remove type definitions that conflicts with module type aliases

--- packages/blog/types.d.ts
+++ packages/blog/types.d.ts
@@ -1,4 +1,2 @@
 // eslint-disable-next-line spaced-comment
 /// <reference types="@docusaurus/module-type-aliases" />
-// eslint-disable-next-line spaced-comment
-/// <reference types="react-scripts" />
