2b816b1c96b5205b0dcf0a4f690c882f167174cf
Sam Zhou <sam@developersam.com>
2020-05-20 18:51:05 -0400

    [queue] Remove queue app (#502)
    Not really using it.

--- packages/queue/src/components/LoadingPage.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactElement } from 'react';
-
-import { CircularProgress } from '@material-ui/core';
-
-export default (): ReactElement => (
-  <div className="simple-page-center">
-    <CircularProgress />
-  </div>
-);
