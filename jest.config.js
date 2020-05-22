e8465dca6a18e7cd3f4eb84ea75c420e3e746cb2
Sam Zhou <sam@developersam.com>
2020-05-22 14:35:09 -0400

    [repo] Remove redundant configurations (#525)

--- jest.config.js
+++ jest.config.js
@@ -1,2 +1,20 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-module.exports = require('./configuration/jest.config.provider');
+const libraries = require('./configuration/libraries.json');
+
+const babelJestPath = require.resolve('babel-jest');
+const identityObjectProxyPath = require.resolve('identity-obj-proxy');
+
+module.exports = {
+  collectCoverageFrom: ['**/*.{js,jsx,ts,tsx}', '!**/*.d.ts', '!**/node_modules/**'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': babelJestPath,
+  },
+  transformIgnorePatterns: [
+    `/node_modules/(?!(${libraries.join('|')})).+\\.js$`,
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  moduleNameMapper: {
+    '^.+\\.module\\.(css|sass|scss)$': identityObjectProxyPath,
+  },
+};
