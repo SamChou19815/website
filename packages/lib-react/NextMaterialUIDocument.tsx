ff5049e73ecd168e37e354b4f2a2f7a44c2325f0
Sam Zhou <sam@developersam.com>
2020-05-23 17:47:33 -0400

    [lib-react] Added `NextMaterialUIDocument` and make apps use it (#533)
    Fix material-ui & next.js across all apps

--- packages/lib-react/NextMaterialUIDocument.tsx
+++ packages/lib-react/NextMaterialUIDocument.tsx
@@ -12,7 +12,7 @@ import Document, {
   NextScript,
 } from 'next/document';
 
-class MyDocument extends Document {
+export default class NextMaterialUIDocument extends Document {
   static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     const sheets = new ServerStyleSheets();
     const originalRenderPage = ctx.renderPage;
@@ -31,16 +31,7 @@ class MyDocument extends Document {
   render(): ReactElement {
     return (
       <Html lang="en">
-        <Head>
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto+Mono:400,500&display=swap"
-          />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
@@ -49,5 +40,3 @@ class MyDocument extends Document {
     );
   }
 }
-
-export default MyDocument;
---
+++ packages/ten/pages/_document.ts
@@ -0,0 +1,3 @@
+import NextMaterialUIDocument from 'lib-react/NextMaterialUIDocument';
+
+export default NextMaterialUIDocument;
