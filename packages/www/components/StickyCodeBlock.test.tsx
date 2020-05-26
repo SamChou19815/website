80ff4a06383eb0f29f6de823adf21abe18e811bc
Sam <sam@developersam.com>
2020-05-26 17:16:13 -0400

    [www] Support dark mode in css

--- packages/www/components/StickyCodeBlock.test.tsx
+++ packages/www/components/StickyCodeBlock.test.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import { Provider as ReactReduxProvider } from 'react-redux';
 import renderer from 'react-test-renderer';
 
+import { store } from '../store';
 import FirstPageCodeBlock from './StickyCodeBlock';
 
 it('FirstPageCodeBlock matches snapshot.', () => {
-  const tree = renderer.create(<FirstPageCodeBlock />).toJSON();
+  const tree = renderer
+    .create(
+      <ReactReduxProvider store={store}>
+        <FirstPageCodeBlock />
+      </ReactReduxProvider>
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
