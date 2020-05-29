18619403e61d689a22f89387ce89cf27c3c10050
Sam Zhou <sam@developersam.com>
2020-05-29 12:10:39 -0400

    [lib-docusaurus] No longer use pnp webpack plugin (#593)
    Alpha 56 has native support for pnp

--- packages/lib-docusaurus-plugin/index.ts
+++ packages/lib-docusaurus-plugin/index.ts
@@ -1,20 +1,9 @@
 import { Plugin } from '@docusaurus/types';
-import PnpWebpackPlugin from 'pnp-webpack-plugin';
 
 const setupPlugin = (): Plugin<void> => ({
   name: 'lib-docusaurus-plugin',
   getClientModules() {
     return [require.resolve('./prism-include-languages')];
-  },
-  configureWebpack() {
-    return {
-      resolve: {
-        plugins: [PnpWebpackPlugin],
-      },
-      resolveLoader: {
-        plugins: [PnpWebpackPlugin.moduleLoader(module)],
-      },
-    };
   },
 });
 
