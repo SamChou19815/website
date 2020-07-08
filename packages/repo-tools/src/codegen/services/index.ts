b96b24acac41d545eefadfd253fabdc14330112b
Sam <sam@developersam.com>
2020-07-08 01:10:08 -0400

    [repo-tools] Make it zero prod dep again

--- packages/repo-tools/src/codegen/services/index.ts
+++ packages/repo-tools/src/codegen/services/index.ts
@@ -1,9 +1,5 @@
-/* eslint-disable no-console */
-
-import { writeFileSync, unlinkSync } from 'fs';
-
-import chalk from 'chalk';
-import glob from 'glob';
+import { spawnSync } from 'child_process';
+import { writeFileSync } from 'fs';
 
 import githubActionsCodegenService from './codegen-github-actions';
 import ignoreFileCodegenService from './codegen-ignore-files';
@@ -15,37 +11,16 @@ const codegenServices = [
   staticJsonCodegenService,
 ];
 
-const resolveGlobPatterns = (pattern: string): Promise<readonly string[]> =>
-  new Promise((resolve, reject) => {
-    glob(pattern, (error, matches) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(matches);
-      }
-    });
-  });
-
-const executeCodegenServices = async (): Promise<void> => {
-  console.log(chalk.cyan('@dev-sam/repo-tools codegen service.\n'));
-  // eslint-disable-next-line no-restricted-syntax
-  for (const codegenService of codegenServices) {
-    const { serviceName, generatedFilenamePattern, generatedCodeContentList } = codegenService;
-    console.group(chalk.green(serviceName));
-
+const executeCodegenServices = (): void => {
+  codegenServices.forEach((codegenService) => {
+    const { generatedFilenamePattern, generatedCodeContentList } = codegenService;
     if (generatedFilenamePattern != null) {
-      // eslint-disable-next-line no-await-in-loop
-      (await resolveGlobPatterns(generatedFilenamePattern)).map((pathToBeRemoved) =>
-        unlinkSync(pathToBeRemoved)
-      );
+      spawnSync('rm', [generatedFilenamePattern], { shell: true });
     }
-
-    generatedCodeContentList.forEach(({ pathForGeneratedCode, generatedCode }, index) => {
-      console.log(`${index + 1}. ${pathForGeneratedCode}`);
-      writeFileSync(pathForGeneratedCode, generatedCode);
-    });
-    console.groupEnd();
-  }
+    generatedCodeContentList.forEach(({ pathForGeneratedCode, generatedCode }) =>
+      writeFileSync(pathForGeneratedCode, generatedCode)
+    );
+  });
 };
 
 export default executeCodegenServices;
