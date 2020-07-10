51045e9595c6fffac66a2567269fcdf4cc445dca
Sam <sam@developersam.com>
2020-07-09 23:07:37 -0400

    [monorail] Tweak compile output name

---
+++ packages/monorail/build.js
@@ -0,0 +1,10 @@
+const { writeFileSync } = require('fs');
+const { join } = require('path');
+
+require('@zeit/ncc')(join(__dirname, 'src', 'index.ts'), {
+  filename: 'monorail',
+  minify: true,
+  sourceMapRegister: false,
+  transpileOnly: true,
+  quiet: true,
+}).then(({ code }) => writeFileSync(join(__dirname, 'bin', 'monorail'), code));
