9416afbda10832673cb2a132952cf3efbaff91a4
Sam Zhou <sam@developersam.com>
2020-07-09 22:18:14 -0400

    [repo-tools] Setup CLI parser (#641)

--- packages/repo-tools/src/index.ts
+++ packages/repo-tools/src/index.ts
@@ -4,6 +4,21 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports,  @typescript-eslint/no-var-requires
 process.chdir(require('./configuration').PROJECT_ROOT_DIRECTORY);
 
+import parseCommandLineArgumentsIntoCommand from './cli-parser';
 import executeCodegenServices from './codegen/services';
 
-executeCodegenServices();
+const main = (): void => {
+  const command = parseCommandLineArgumentsIntoCommand();
+
+  switch (command.type) {
+    case 'CODEGEN':
+      executeCodegenServices();
+      return;
+    case 'SYNC':
+      return;
+    default:
+      throw new Error();
+  }
+};
+
+main();
