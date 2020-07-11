b7cbd8abb536d61520ed9018ea61075bfec10319
Sam <sam@developersam.com>
2020-07-10 22:54:41 -0400

    Fix clearing

--- vscode-disable-generated-files-edit/index.ts
+++ vscode-disable-generated-files-edit/index.ts
@@ -1,3 +1,5 @@
+/** eslint-disable */
+
 import * as vscode from 'vscode';
 
 const disabledEditDecorationType = vscode.window.createTextEditorDecorationType({
@@ -13,17 +15,31 @@ const updateGeneratedHoverForEditor = (editor: vscode.TextEditor | undefined): v
   if (editor == null) {
     return;
   }
-  if (editor.document.getText().includes('@generated')) {
+  if (editor.document.getText().includes('@' + 'generated')) {
     editor.setDecorations(disabledEditDecorationType, [
       {
         range: new vscode.Range(0, 0, Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER),
         hoverMessage: 'Do not edit generated code or risk failing CI jobs.',
       },
     ]);
+  } else {
+    editor.setDecorations(disabledEditDecorationType, []);
   }
 };
 
-export function activate(): void {
-  vscode.window.onDidChangeActiveTextEditor((editor) => updateGeneratedHoverForEditor(editor));
+export function activate(context: vscode.ExtensionContext): void {
+  context.subscriptions.push(
+    vscode.window.onDidChangeActiveTextEditor((editor) => updateGeneratedHoverForEditor(editor)),
+    vscode.workspace.onDidChangeTextDocument(({ document }) => {
+      const { activeTextEditor } = vscode.window;
+      if (
+        activeTextEditor != null &&
+        activeTextEditor.document.uri.toString() === document.uri.toString()
+      ) {
+        updateGeneratedHoverForEditor(activeTextEditor);
+      }
+    })
+  );
+
   updateGeneratedHoverForEditor(vscode.window.activeTextEditor);
 }
