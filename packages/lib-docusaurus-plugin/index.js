f3045dfac6802bb33ca7ac236908b9af36d30861
Sam Zhou <sam@developersam.com>
2020-07-12 18:28:13 -0400

    [repo] Un-styleignore packages/lib-docusaurus-plugin/index.js (#683)
    It's no longer a generated file

--- packages/lib-docusaurus-plugin/index.js
+++ packages/lib-docusaurus-plugin/index.js
@@ -2,10 +2,10 @@
 
 /** @returns {import('@docusaurus/types').Plugin<void>} */
 const setupPlugin = () => ({
-    name: 'lib-docusaurus-plugin',
-    getClientModules() {
-        return [require.resolve('./prism-include-languages')];
-    },
+  name: 'lib-docusaurus-plugin',
+  getClientModules() {
+    return [require.resolve('./prism-include-languages')];
+  },
 });
 
 module.exports = setupPlugin;
