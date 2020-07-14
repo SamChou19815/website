6720c227b812f7954f65fd2ad2e3729797e15a4a
Sam Zhou <sam@developersam.com>
2020-07-13 22:23:35 -0400

    [www] Remove all snapshot tests (#707)
    These stupid tests don't help me catch anything. Due to them, I needlessly failed a lot of CI checks.

--- packages/www/components/WebTerminal/index.test.tsx
+++ packages/www/components/WebTerminal/index.test.tsx
@@ -2,33 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { act } from 'react-dom/test-utils';
-import renderer from 'react-test-renderer';
 
-import Default, { WebTerminal } from '.';
-
-const ignore = () => {};
-
-it(`WebTerminal(shown) matches snapshot.`, () => {
-  const tree = renderer.create(<WebTerminal shown onTrigger={ignore} />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
-
-it(`WebTerminal(hidden) matches snapshot.`, () => {
-  const tree = renderer.create(<WebTerminal shown={false} onTrigger={ignore} />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
-
-it(`Default matches snapshot.`, () => {
-  const tree = renderer.create(<Default />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
+import WebTerminal from '.';
 
 it('Can trigger', () => {
   const container = document.createElement('div');
   document.body.appendChild(container);
 
   act(() => {
-    ReactDOM.render(<Default />, container);
+    ReactDOM.render(<WebTerminal />, container);
   });
   const buttonNode = document.querySelector('button') as HTMLButtonElement;
 
