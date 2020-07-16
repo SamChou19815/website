2cd98f6ea72a99ac84713576ef29bc32b5ee985b
Sam Zhou <sam@developersam.com>
2020-07-15 21:28:42 -0400

    [monorail] Enforce immutable install on CI (#721)

--- packages/monorail/src/codegen/github-actions/github-actions-yarn-workspaces.ts
+++ packages/monorail/src/codegen/github-actions/github-actions-yarn-workspaces.ts
@@ -25,7 +25,7 @@ export const yarnWorkspaceBoilterplateSetupSteps = [
   GITHUB_ACTIONS_CHECKOUT_STEP,
   GITHUB_ACTIONS_SETUP_NODE_STEP,
   GITHUB_ACTIONS_USE_YARN_CACHE_STEP,
-  githubActionJobRunStep('Yarn Install', 'yarn install'),
+  githubActionJobRunStep('Yarn Install', 'yarn install --immutable'),
 ];
 
 const yarnWorkspaceGetDependencyPaths = (workspace: string): readonly string[] => [
