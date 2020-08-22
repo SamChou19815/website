52562b7e9b4dda7293c00736ffcb08af0be89ab7
Sam <sam@developersam.com>
2020-08-21 22:52:41 -0400

    [wiki] Separate metadata and content editor

--- packages/wiki/src/app/MarkdownInputCard.tsx
+++ packages/wiki/src/app/MarkdownInputCard.tsx
@@ -8,19 +8,37 @@ import styles from './MarkdownInputCard.module.css';
 import PrismCodeEditor from 'lib-react/PrismCodeEditor';
 
 type Props = {
+  readonly title: string;
   readonly code: string;
   readonly className?: string;
+  readonly onTitleChange: (title: string) => void;
   readonly onCodeChange: (code: string) => void;
   readonly onSubmit: (code: string) => void;
 };
 
-const MarkdownInputCard = ({ code, className, onCodeChange, onSubmit }: Props): ReactElement => {
+const MarkdownInputCard = ({
+  title,
+  code,
+  className,
+  onTitleChange,
+  onCodeChange,
+  onSubmit,
+}: Props): ReactElement => {
   const theme = usePrismTheme();
 
   return (
     <div className={clsx('card', className)}>
       <div className="card__header">
         <h2>Markdown Editor</h2>
+      </div>
+      <div className="card__body">
+        <input
+          className="text-input"
+          type="text"
+          value={title}
+          placeholder="Title"
+          onChange={(event) => onTitleChange(event.currentTarget.value)}
+        />
       </div>
       <div
         className={clsx('card__body', styles.EditorCardContainer)}
