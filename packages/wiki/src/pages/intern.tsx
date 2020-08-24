3e7426e9d83c417b58dbefbf2d4d4137d86f0e5a
Sam <sam@developersam.com>
2020-08-24 18:40:00 -0400

    [wiki] Do not hardcode App as entry point in FirebaseLoginAppBarrier

--- packages/wiki/src/pages/intern.tsx
+++ packages/wiki/src/pages/intern.tsx
@@ -6,13 +6,16 @@ import Layout from '@theme/Layout';
 // Lazy import is necessary for conditional execution below,
 // since firebase/app code cannot be executed in SSR environments.
 const FirebaseLoginAppBarrier = lazy(() => import('../app/FirebaseLoginAppBarrier'));
+const App = lazy(() => import('../app/App'));
 
 export default function InternTierAccessEntryPoint(): ReactElement {
   return (
     <Layout title="@dev-sam internals" description="Restricted-access internal portal">
       {ExecutionEnvironment.canUseDOM ? (
         <Suspense fallback={null}>
-          <FirebaseLoginAppBarrier isDataLoaded={true} dataLoader={() => {}} />
+          <FirebaseLoginAppBarrier>
+            <App />
+          </FirebaseLoginAppBarrier>
         </Suspense>
       ) : (
         <div />
