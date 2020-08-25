5722c5102a417be4f4cb0018ae45f77547db6772
Sam <sam@developersam.com>
2020-08-25 19:06:39 -0400

    [www] Simplify next config

--- packages/www/next.config.js
+++ packages/www/next.config.js
@@ -1,7 +1,1 @@
-module.exports = require('next-transpile-modules')(['lib-react'])({
-  webpack(config) {
-    // eslint-disable-next-line no-param-reassign
-    config.node = { fs: 'empty', child_process: 'empty' };
-    return config;
-  },
-});
+module.exports = require('next-transpile-modules')(['lib-react']);
