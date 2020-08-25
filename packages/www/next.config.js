ca6c49d3444c5e7254f82b0342cf39eb5891b757
Sam <sam@developersam.com>
2020-08-25 18:55:01 -0400

    [www] Move next config override out of configuration
    Now `www` is the only project that is using `next`.

--- packages/www/next.config.js
+++ packages/www/next.config.js
@@ -1,1 +1,7 @@
-module.exports = require('../../configuration/next-config-overrider')();
+module.exports = require('next-transpile-modules')(['lib-react'])({
+  webpack(config) {
+    // eslint-disable-next-line no-param-reassign
+    config.node = { fs: 'empty', child_process: 'empty' };
+    return config;
+  },
+});
