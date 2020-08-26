0b8167b59b38fdb2e0b4db5dc314324cc0e8f0dd
Sam <sam@developersam.com>
2020-08-26 19:38:23 -0400

    [monorail] Eliminate repo tools configuration
    Hardcode deploymentSecrets for now. They will eventually be moved to codegen configurations

--- packages/monorail/src/configuration.ts
+++ packages/monorail/src/configuration.ts
@@ -1,24 +1,7 @@
 import { existsSync, readFileSync, lstatSync } from 'fs';
 import { join, dirname, relative } from 'path';
 
-import { assertIsStringArray, assertHasFields } from './validator';
-
-type RepoToolsConfiguration = {
-  readonly deploymentSecrets: readonly string[];
-};
-
-const parseRepoToolsConfiguration = (json: unknown): RepoToolsConfiguration => {
-  const { deploymentSecrets } = assertHasFields(
-    'repoToolsConfiguration',
-    ['deploymentSecrets'],
-    json
-  );
-  return {
-    deploymentSecrets: assertIsStringArray('deploymentSecrets', deploymentSecrets),
-  };
-};
-
-const loadRepoToolsConfigurationAndFindRoot = (): readonly [string, RepoToolsConfiguration] => {
+const findMonorepoRoot = (): string => {
   let configurationDirectory = process.cwd();
   while (configurationDirectory !== '/') {
     const configurationPath = join(configurationDirectory, 'package.json');
@@ -26,20 +9,14 @@ const loadRepoToolsConfigurationAndFindRoot = (): readonly [string, RepoToolsCon
       const packageJson = JSON.parse(readFileSync(configurationPath).toString());
       // Detect root Yarn workspace
       if (Array.isArray(packageJson.workspaces)) {
-        return [
-          configurationDirectory,
-          parseRepoToolsConfiguration(packageJson.repoToolsConfiguration),
-        ];
+        return configurationDirectory;
       }
     }
     configurationDirectory = dirname(configurationDirectory);
   }
-  throw new Error('No configuration found. Abort!');
+  throw new Error('No root package.json found. Abort!');
 };
 
-export const [
-  PROJECT_ROOT_DIRECTORY,
-  PROJECT_CONFIGURATION,
-] = loadRepoToolsConfigurationAndFindRoot();
+export const PROJECT_ROOT_DIRECTORY = findMonorepoRoot();
 
 export const MONORAIL_BINARY_PATH = relative(PROJECT_ROOT_DIRECTORY, process.argv[1]);
