65feb8497637b9a70957b0d755d73004538ab84f
Sam Zhou <sam@developersam.com>
2020-08-26 16:51:54 -0400

    [monorail] Query changes files for incremental compile (#871)

--- packages/monorail/src/incremental-compile/index.ts
+++ packages/monorail/src/incremental-compile/index.ts
@@ -1,64 +1,55 @@
 /* eslint-disable no-console */
 
-import { spawnSync, spawn } from 'child_process';
+import { spawn } from 'child_process';
+import { join } from 'path';
 
 import chalk from 'chalk';
 
-import workspaceNeedRebuild from '../infrastructure/need-rebuild-checker';
-import {
-  getYarnWorkspacesInTopologicalOrder,
-  getYarnWorkspaceHasCompileScript,
-  getYarnWorkspaceLocation,
-} from '../infrastructure/yarn-workspace-dependency-analysis';
-import {
-  getIncrementalCompileLastRunTime,
-  setIncrementalCompileLastRunTime,
-} from './incremental-compile-cache';
+import { workspaceInformation } from '../infrastructure/yarn-workspace-dependency-analysis';
+
+import queryChangedFilesSince from 'lib-changed-files';
+import runIncrementalTasks, { IncrementalTaskSpecification } from 'lib-incremental';
+
+const incrementalTaskSpecification: IncrementalTaskSpecification = {
+  lastestKnownGoodRunTimeFilename: join('.monorail', 'compile-cache.json'),
+  needRerun: async (latestKnownGoodRerunTime) => {
+    const allWorkspaces = await Promise.all(
+      Array.from(workspaceInformation.entries()).map(
+        async ([workspaceName, { workspaceLocation, hasCompileScript }]) => {
+          if (!hasCompileScript) return [workspaceName, false] as const;
+          const { changedFiles, deletedFiles } = await queryChangedFilesSince(
+            latestKnownGoodRerunTime[workspaceName] ?? 0,
+            workspaceLocation
+          );
+          return [workspaceName, changedFiles.length + deletedFiles.length !== 0] as const;
+        }
+      )
+    );
+
+    const targets = allWorkspaces
+      .filter(([, needRebuild]) => needRebuild)
+      .map(([workspace]) => workspace);
+    if (targets.length !== 0) {
+      console.log(chalk.yellow(`[${targets.join(', ')}] needs to be re-compiled!`));
+    }
+    return targets;
+  },
+  rerun: async (workspace) => {
+    console.log(`Compiling \`${workspace}\`...`);
+    const childProcess = spawn('yarn', ['workspace', workspace, 'compile']);
+    return await new Promise<boolean>((resolve) => {
+      childProcess.on('exit', (code) => resolve(code === 0));
+    });
+  },
+};
 
 const incrementalCompile = async (): Promise<void> => {
   console.log(chalk.blue('--- Monorail Incremental Compile Service ---'));
-
-  const lastRunTime = getIncrementalCompileLastRunTime();
-
-  const needToRecompileLocalCheck = (workspace: string): boolean =>
-    workspaceNeedRebuild(workspace, [], lastRunTime);
-  const needToRecompileCICheck = (workspace: string): boolean =>
-    spawnSync('git', ['diff', 'HEAD^', 'HEAD', '--name-only', getYarnWorkspaceLocation(workspace)])
-      .stdout.toString()
-      .trim().length > 0;
-  const needToRecompileCheck = process.env.CI ? needToRecompileCICheck : needToRecompileLocalCheck;
-
-  const workspacesToCompile = getYarnWorkspacesInTopologicalOrder()
-    .filter(getYarnWorkspaceHasCompileScript)
-    .filter(needToRecompileCheck);
-  if (workspacesToCompile.length === 0) {
-    console.log(chalk.green('[✓] Nothing needs to be recompiled!'));
-    return;
-  }
-  console.group(chalk.yellow(`[${workspacesToCompile.join(', ')}] needs to be re-compiled!`));
-
-  const successfulStatus = await Promise.all(
-    workspacesToCompile.map((workspace) => {
-      console.log(`Compiling \`${workspace}\`...`);
-      const childProcess = spawn('yarn', ['workspace', workspace, 'compile']);
-      return new Promise<readonly [string, boolean]>((resolve) => {
-        childProcess.on('exit', (code) => resolve([workspace, code === 0] as const));
-      });
-    })
-  );
-
-  console.groupEnd();
-
-  const failedWorkspacesRuns = successfulStatus
-    .filter(([, successful]) => !successful)
-    .map(([name]) => name);
-
+  const failedWorkspacesRuns = await runIncrementalTasks(incrementalTaskSpecification);
   if (failedWorkspacesRuns.length === 0) {
     console.log(chalk.green('[✓] All workspaces have been successfully compiled!'));
-    setIncrementalCompileLastRunTime();
     return;
   }
-
   throw new Error(`[x] [${failedWorkspacesRuns.join(', ')}] failed to exit with 0`);
 };
 
