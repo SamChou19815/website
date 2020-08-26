bba6a56be8dfff6c5df38fee3d1e8a45b5e69cd7
Sam Zhou <sam@developersam.com>
2020-08-26 15:13:52 -0400

    [monorail] Reimplement incremental rebuild with new library (#868)

---
+++ packages/lib-incremental/index.ts
@@ -0,0 +1,63 @@
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs';
+import { dirname } from 'path';
+
+/** Task ID to lastest good run time. */
+type MutableLastestKnownGoodRunTime = Record<string, number | undefined>;
+export type LastestKnownGoodRunTime = Readonly<MutableLastestKnownGoodRunTime>;
+
+export interface IncrementalTaskSpecification {
+  readonly lastestKnownGoodRunTimeFilename: string;
+  /**
+   * @returns a list of task ID that needs to be rerun.
+   * Usually, implementation of this function will consider the set of changed files.
+   */
+  readonly needRerun: (
+    lastestKnownGoodRunTime: LastestKnownGoodRunTime
+  ) => Promise<readonly string[]>;
+  /** Run the job and returns whether a run is successful. */
+  readonly rerun: (taskID: string) => Promise<boolean>;
+}
+
+/**
+ * The incremental task running framework.
+ * It doesn't care about which tasks to rerun and how to rerun them. These information is provided by
+ * the `specification` object.
+ * Instead, it will maintain latest known good run time to help decide rerunning jobs.
+ *
+ * @param specification
+ * @returns task IDs of failed jobs.
+ */
+const runIncrementalTasks = async (
+  specification: IncrementalTaskSpecification
+): Promise<readonly string[]> => {
+  const lastestKnownGoodRunTime: MutableLastestKnownGoodRunTime = existsSync(
+    specification.lastestKnownGoodRunTimeFilename
+  )
+    ? JSON.parse(readFileSync(specification.lastestKnownGoodRunTimeFilename).toString())
+    : ({} as MutableLastestKnownGoodRunTime);
+
+  const tasksToRun = await specification.needRerun(lastestKnownGoodRunTime);
+  const statusList = await Promise.all(
+    tasksToRun.map(async (taskID) => [taskID, await specification.rerun(taskID)] as const)
+  );
+
+  const latestRunTime = new Date().getTime();
+  const failed: string[] = [];
+  statusList.forEach(([taskID, successful]) => {
+    if (successful) {
+      lastestKnownGoodRunTime[taskID] = latestRunTime;
+    } else {
+      failed.push(taskID);
+    }
+  });
+
+  mkdirSync(dirname(specification.lastestKnownGoodRunTimeFilename), { recursive: true });
+  writeFileSync(
+    specification.lastestKnownGoodRunTimeFilename,
+    JSON.stringify(lastestKnownGoodRunTime, undefined, 2)
+  );
+
+  return failed;
+};
+
+export default runIncrementalTasks;
--- packages/monorail/src/cached-build/index.ts
+++ packages/monorail/src/cached-build/index.ts
@@ -1,40 +1,42 @@
 /* eslint-disable no-console */
 
 import { spawnSync } from 'child_process';
+import { join } from 'path';
 
 import chalk from 'chalk';
 
 import cachedBuildTargetDeterminator from './cached-build-target-determinator';
 
-const cachedBuild = async (): Promise<void> => {
-  console.log(chalk.blue('--- Monorail Cached Build Service ---'));
-  const targets = cachedBuildTargetDeterminator();
-  if (targets.length === 0) {
-    console.log(chalk.green('[✓] No need to rebuild!'));
-    return;
-  }
+import runIncrementalTasks, { IncrementalTaskSpecification } from 'lib-incremental';
 
-  console.group(chalk.yellow(`[${targets.join(', ')}] needs to be rebuilt!`));
-  const successfulStatus = targets.map((workspace) => {
+const incrementalTaskSpecification: IncrementalTaskSpecification = {
+  lastestKnownGoodRunTimeFilename: join('.monorail', 'build-cache.json'),
+  needRerun: async () => {
+    const targets = cachedBuildTargetDeterminator();
+    if (targets.length === 0) {
+      console.log(chalk.green('[✓] No need to rebuild!'));
+      return [];
+    }
+    console.log(chalk.yellow(`[${targets.join(', ')}] needs to be rebuilt!`));
+    return targets;
+  },
+  rerun: async (workspace) => {
     console.log(`Rebuiding \`${workspace}\`...`);
-    return [
-      workspace,
+    return (
       spawnSync('yarn', ['workspace', workspace, 'codegen'], {
         stdio: ['inherit', process.env.INCLUDE_ERROR ? 'inherit' : 'ignore', 'inherit'],
-      }).status === 0,
-    ] as const;
-  });
-  console.groupEnd();
+      }).status === 0
+    );
+  },
+};
 
-  const failedWorkspacesRuns = successfulStatus
-    .filter(([, successful]) => !successful)
-    .map(([name]) => name);
-
+const cachedBuild = async (): Promise<void> => {
+  console.log(chalk.blue('--- Monorail Cached Build Service ---'));
+  const failedWorkspacesRuns = await runIncrementalTasks(incrementalTaskSpecification);
   if (failedWorkspacesRuns.length === 0) {
     console.log(chalk.green('[✓] All workspaces have been successfully rebuilt!'));
     return;
   }
-
   throw new Error(`[x] [${failedWorkspacesRuns.join(', ')}] failed to exit with 0`);
 };
 
