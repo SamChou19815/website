e5899283386065581a3a37df9e816e9bf82e2ae8
Sam Zhou <sam@developersam.com>
2020-08-28 13:40:44 -0400

    [docusaurus] Adopt alpha-62 (#901)
    * Bump docusaurus to alpha-62
    
    * Unbreak blog
    
    * Use classic theme types
    
    * Unbreak compile

--- packages/samlang/src/types.d.ts
+++ packages/samlang/src/types.d.ts
@@ -1,3 +1,4 @@
 /// <reference types="react" />
 /// <reference types="@docusaurus/module-type-aliases" />
+/// <reference types="@docusaurus/theme-classic" />
 /// <reference types="lib-react/types" />
