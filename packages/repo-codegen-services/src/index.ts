11de8df335170d2efed61dbe9c322d45b05b2edf
Sam Zhou <sam@developersam.com>
2020-09-12 12:18:07 -0400

    [codegen] Do not codegen ignore files (#934)
    Right now all files in .yarn directory that is not gitignored has extension cjs, so they won't be bothered by eslint and prettier. Last diff makes the files in bin extensionless. Therefore, at this point, all ignore files are unified again and codegen for those files are no longer necessary.

--- packages/repo-codegen-services/src/index.ts
+++ packages/repo-codegen-services/src/index.ts
@@ -1,32 +1,3 @@
 import githubActionsCodegenService from './github-actions-codegen-service';
 
-import { CodegenService } from 'lib-codegen';
-
-const ignoreFileCodegenService: CodegenService = {
-  name: 'Ignore Files Codegen',
-  sourceFileIsRelevant: (sourceFilename) => sourceFilename === '.gitignore',
-  run: (_, gitignoreContent) => {
-    const styleIgnoreContent = `# @${'generated'}
-
-${gitignoreContent}
-# additions
-.yarn
-**/bin/
-**/out/
-**/build/
-**/dist/
-`;
-
-    return [
-      { outputFilename: '.eslintignore', outputContent: styleIgnoreContent },
-      { outputFilename: '.prettierignore', outputContent: styleIgnoreContent },
-    ];
-  },
-};
-
-const codegenServices: readonly CodegenService[] = [
-  githubActionsCodegenService,
-  ignoreFileCodegenService,
-];
-
-export = codegenServices;
+export = [githubActionsCodegenService];
