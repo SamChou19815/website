7db50ec8b2b8f2bd6139398c1042e1ef43682c87
Sam Zhou <sam@developersam.com>
2020-10-02 14:16:43 -0400

    [repo] Adopt yarn 2.3 (#958)

--- .yarn/sdks/typescript/lib/tsserver.js
+++ .yarn/sdks/typescript/lib/tsserver.js
@@ -2,7 +2,7 @@
 
 const { existsSync } = require(`fs`);
 const { createRequire, createRequireFromPath } = require(`module`);
-const { resolve } = require(`path`);
+const { resolve, dirname } = require(`path`);
 
 const relPnpApiPath = '../../../../.pnp.js';
 
@@ -18,12 +18,24 @@ const moduleWrapper = (tsserver) => {
 
   const Session = tsserver.server.Session;
   const { onMessage: originalOnMessage, send: originalSend } = Session.prototype;
+  let isVSCode = false;
 
   return Object.assign(Session.prototype, {
     onMessage(/** @type {string} */ message) {
+      const parsedMessage = JSON.parse(message);
+
+      if (
+        parsedMessage != null &&
+        typeof parsedMessage === 'object' &&
+        parsedMessage.arguments &&
+        parsedMessage.arguments.hostInfo === 'vscode'
+      ) {
+        isVSCode = true;
+      }
+
       return originalOnMessage.call(
         this,
-        JSON.stringify(JSON.parse(message), (key, value) => {
+        JSON.stringify(parsedMessage, (key, value) => {
           return typeof value === 'string' ? removeZipPrefix(value) : value;
         })
       );
@@ -45,20 +57,24 @@ const moduleWrapper = (tsserver) => {
     // We add the `zip:` prefix to both `.zip/` paths and virtual paths
     if (
       isAbsolute(str) &&
-      !str.match(/^zip:/) &&
+      !str.match(/^\^zip:/) &&
       (str.match(/\.zip\//) || str.match(/\$\$virtual\//))
     ) {
       // Absolute VSCode `Uri.fsPath`s need to start with a slash.
       // VSCode only adds it automatically for supported schemes,
       // so we have to do it manually for the `zip` scheme.
-      return `zip:${str.replace(/^\/?/, `/`)}`;
+      // The path needs to start with a caret otherwise VSCode doesn't handle the protocol
+      // https://github.com/microsoft/vscode/issues/105014#issuecomment-686760910
+      return `${isVSCode ? '^' : ''}zip:${str.replace(/^\/?/, `/`)}`;
     } else {
       return str;
     }
   }
 
   function removeZipPrefix(str) {
-    return process.platform === 'win32' ? str.replace(/^zip:\//, ``) : str.replace(/^zip:/, ``);
+    return process.platform === 'win32'
+      ? str.replace(/^\^?zip:\//, ``)
+      : str.replace(/^\^?zip:/, ``);
   }
 };
 
