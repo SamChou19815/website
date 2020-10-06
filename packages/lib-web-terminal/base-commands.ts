e0fd74bc9f96bb80847ad81f5f1521549dabf253
Sam Zhou <sam@developersam.com>
2020-10-05 20:04:12 -0400

    [lib-web-terminal] Simplify terminal setup (#969)

--- packages/lib-web-terminal/base-commands.ts
+++ packages/lib-web-terminal/base-commands.ts
@@ -8,41 +8,55 @@ import {
   showFiles,
 } from 'lib-in-memory-filesystem';
 
-const cat = (...paths: string[]): string => {
-  try {
-    return showFiles(getFilesystemState(), paths);
-  } catch (exception) {
-    return exception.message;
-  }
-};
-
-const cd = (path: string | undefined): string | void => {
-  try {
-    setFilesystemState(changeDirectory(getFilesystemState(), path || '/'));
-    return undefined;
-  } catch (exception) {
-    return exception.message;
-  }
-};
-
-const echo = (...inputs: string[]): string => inputs.join(' ');
-
-const ls = (...paths: string[]): string => {
-  try {
-    return listFiles(getFilesystemState(), paths);
-  } catch (exception) {
-    return exception.message;
-  }
-};
-
-const pwd = (): string => currentDirectoryPath(getFilesystemState());
-
 const baseCommands: Commands = {
-  cat: { fn: cat, description: 'Concatenate and print files', usage: 'cat [path1] [path2] ...' },
-  cd: { fn: cd, description: 'Change current directory.', usage: 'cd [path]' },
-  echo: { fn: echo, description: 'Print back the arguments.', usage: 'echo [foo] [bar] ...' },
-  ls: { fn: ls, description: 'List directory contents.', usage: 'ls [path1] [path2] ...' },
-  pwd: { fn: pwd, description: 'Print currrent absolute directory.' },
+  cat: {
+    fn: (...paths: string[]): string => {
+      try {
+        return showFiles(getFilesystemState(), paths);
+      } catch (exception) {
+        return exception.message;
+      }
+    },
+    description: 'Concatenate and print files',
+    usage: 'cat [path1] [path2] ...',
+  },
+
+  cd: {
+    fn: (path: string | undefined): string | void => {
+      try {
+        setFilesystemState(changeDirectory(getFilesystemState(), path || '/'));
+        return undefined;
+      } catch (exception) {
+        return exception.message;
+      }
+    },
+    description: 'Change current directory.',
+    usage: 'cd [path]',
+  },
+
+  echo: {
+    fn: (...inputs: string[]): string => inputs.join(' '),
+    description: 'Print back the arguments.',
+    usage: 'echo [foo] [bar] ...',
+  },
+
+  ls: {
+    fn: (...paths: string[]): string => {
+      try {
+        return listFiles(getFilesystemState(), paths);
+      } catch (exception) {
+        return exception.message;
+      }
+    },
+    description: 'List directory contents.',
+    usage: 'ls [path1] [path2] ...',
+  },
+
+  pwd: {
+    fn: (): string => currentDirectoryPath(getFilesystemState()),
+    description: 'Print currrent absolute directory.',
+    usage: 'pwd',
+  },
 };
 
 export default baseCommands;
