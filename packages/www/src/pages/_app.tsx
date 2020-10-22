7078ff29280363e09e8de2053bc87a20490a9eb8
Sam Zhou <sam@developersam.com>
2020-10-21 20:28:34 -0400

    [www] Use Megan's art for og:image meta property (#986)

--- packages/www/src/pages/_app.tsx
+++ packages/www/src/pages/_app.tsx
@@ -37,7 +37,7 @@ const MaterialUIApp = (props: AppProps): ReactElement => {
         <meta property="og:title" content="Developer Sam" />
         <meta property="og:type" content="profile" />
         <meta property="og:url" content="https://developersam.com/" />
-        <meta property="og:image" content="https://developersam.com/timeline/fb-hacker-way.jpg" />
+        <meta property="og:image" content="https://developersam.com/sam-by-megan-3-square.png" />
         <meta property="og:description" content={description} />
         <meta name="author" content="Developer Sam" />
         <link rel="canonical" href="https://developersam.com/" />
