5ebce38d1bed6c36de79277de85490bc1493713e
Sam Zhou <sam@developersam.com>
2020-11-11 21:00:51 -0500

    [github-contribution-alert] Make response typed (#998)

--- packages/github-contribution-alert/index.ts
+++ packages/github-contribution-alert/index.ts
@@ -15,6 +15,21 @@ const graphQLClient = new GraphQLClient('https://api.github.com/graphql', {
   headers: { authorization: `Bearer ${functions.config().github_contribution_alert.github_token}` },
 });
 
+type GitHubGraphQLResponse = {
+  readonly user: {
+    readonly contributionsCollection: {
+      readonly contributionCalendar: {
+        readonly weeks: readonly {
+          readonly contributionDays: readonly {
+            readonly date: string;
+            readonly contributionCount: number;
+          }[];
+        }[];
+      };
+    };
+  };
+};
+
 const numberOfContributionToday = async (githubUser: string): Promise<number> => {
   // The system is built for a small set of users in NY, at least in recent future.
   const currentTimeInNewYork = DateTime.local().setZone('America/New_York');
@@ -22,24 +37,27 @@ const numberOfContributionToday = async (githubUser: string): Promise<number> =>
   const todayInNYEnd = currentTimeInNewYork.endOf('day');
 
   // Using Sam's PAT, which makes the query timezone in NY.
-  const contributionDays: readonly {
-    readonly date: string;
-    readonly contributionCount: number;
-  }[] = await graphQLClient
-    .request(
+  const contributionDays = await graphQLClient
+    .request<GitHubGraphQLResponse>(
       `query {
   user(login: "${githubUser}") {
     contributionsCollection(from: "${todayInNYStart.toISO()}", to: "${todayInNYEnd.toISO()}") {
-      contributionCalendar { weeks { contributionDays { date contributionCount } } }
+      contributionCalendar {
+        weeks {
+          contributionDays {
+            date
+            contributionCount
+          }
+        }
+      }
     }
   }
 }`
     )
-    .then(
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      (data: any) =>
-        data.user.contributionsCollection.contributionCalendar.weeks[0].contributionDays
-    );
+    .then((data) => {
+      console.log(data);
+      return data.user.contributionsCollection.contributionCalendar.weeks[0].contributionDays;
+    });
 
   return (
     contributionDays.find(({ date }) => currentTimeInNewYork.toISODate() === date)
