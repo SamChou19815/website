2d98eb8e2edab82b2fc5a42466e19c7df1507b3e
Sam <sam@developersam.com>
2020-11-18 16:10:03 -0500

    [docusaurus] Bump docusaurus

--- packages/lib-docusaurus-plugin/index.js
+++ packages/lib-docusaurus-plugin/index.js
@@ -1,13 +1,14 @@
 // @ts-check
+const path = require('path');
 
 /** @returns {import('@docusaurus/types').Plugin<void>} */
 const setupPlugin = () => ({
   name: 'lib-docusaurus-plugin',
+  getThemePath() {
+    return path.join(__dirname, 'theme');
+  },
   getClientModules() {
-    return [
-      require.resolve('./prism-include-languages'),
-      require.resolve('lib-react/PrismCodeBlock.css'),
-    ];
+    return [require.resolve('lib-react/PrismCodeBlock.css')];
   },
 });
 
