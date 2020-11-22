13b42ebe850299aa75a2ac639d8fc60be2e4b64d
Sam <sam@developersam.com>
2020-11-21 21:40:06 -0500

    [ten] Color the big square after win
    Can't get proxy to work. Guess I will test in prod.

--- packages/ten/docusaurus-infima-css-plugin.js
+++
@@ -1,8 +0,0 @@
-const setupPlugin = () => ({
-  name: 'docusaurus-infima-css-plugin',
-  getClientModules() {
-    return [require.resolve('infima/dist/css/default/default.css')];
-  },
-});
-
-module.exports = setupPlugin;
