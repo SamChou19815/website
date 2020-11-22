583c151cc7b84ab3cae0ac81fe4f12f3388839e6
Sam Zhou <sam@developersam.com>
2020-11-22 16:32:14 -0500

    [ten] Factor out AI logic into PlayAgainstAIGameCard (#1027)

--- packages/ten/src/components/StatefulGameCard.tsx
+++ packages/ten/src/components/StatefulGameCard.tsx
@@ -1,21 +1,17 @@
-import React, { ReactElement, Dispatch, SetStateAction, useState } from 'react';
+import React, { ReactElement, useState } from 'react';
 
 import {
   Move,
   Board,
   emptyBoard,
-  boardToJson,
   getGameStatus,
   makeMove,
   makeMoveWithoutCheck,
 } from '../game/board';
 import type { GameState, GameStates, GameStatus } from '../game/game-state';
-import type { MctsResponse } from '../game/mcts';
 import GameCard from './GameCard';
 
 import { checkNotNull } from 'lib-common';
-
-const initialGameState: GameState = { board: emptyBoard, status: 'PLAYER_MOVE' };
 
 const computeCanShowGameStarterButtons = (gameStates: GameStates): boolean => {
   switch (gameStates.currentState.status) {
@@ -30,69 +26,46 @@ const computeCanShowGameStarterButtons = (gameStates: GameStates): boolean => {
   }
 };
 
-const aiResponder = async (board: Board): Promise<GameState> =>
-  fetch('/api/respond', { method: 'POST', body: JSON.stringify(boardToJson(board)) })
-    .then((resp): Promise<MctsResponse> => resp.json())
-    .then((json) => {
-      const { move, winningPercentage, simulationCounter } = json;
-      const newBoardAfterAI = makeMoveWithoutCheck(board, move);
-      const gameStatus = getGameStatus(newBoardAfterAI);
-      let newStatus: GameStatus;
-      if (gameStatus === 1) {
-        newStatus = 'BLACK_WINS';
-      } else if (gameStatus === -1) {
-        newStatus = 'WHITE_WINS';
-      } else {
-        newStatus = 'PLAYER_MOVE';
-      }
-      return {
-        board: newBoardAfterAI,
-        highlightedCell: move,
-        status: newStatus,
-        aiInfo: {
-          aiWinningProbability: winningPercentage,
-          aiNumberOfSimulations: simulationCounter,
-        },
-      };
-    });
-
-const clickCellCallback = (
-  board: Board,
-  move: Move,
-  setGameStates: Dispatch<SetStateAction<GameStates>>,
-  otherPlayerResponder: (b: Board) => Promise<GameState>
-): void => {
-  const newBoard = makeMove(board, move);
-  if (newBoard === null) {
-    setGameStates((previousState) => ({
-      ...previousState,
-      currentState: { ...previousState.currentState, status: 'ILLEGAL_MOVE' },
-    }));
-    return;
-  }
-  const gameStatus = getGameStatus(newBoard);
-  let newStatus: GameStatus;
-  if (gameStatus === 1) {
-    newStatus = 'BLACK_WINS';
-  } else if (gameStatus === -1) {
-    newStatus = 'WHITE_WINS';
-  } else {
-    newStatus = 'AI_MOVE';
-  }
-  setGameStates((previousState) => ({
-    previousState: {
-      ...previousState,
-      currentState: { ...previousState.currentState, status: 'PLAYER_MOVE' },
-    },
-    currentState: { board: newBoard, highlightedCell: move, status: newStatus },
-  }));
-  otherPlayerResponder(newBoard).then((currentState) =>
-    setGameStates((previousState) => ({ ...previousState, currentState }))
-  );
+type Props = {
+  readonly initialGameState: GameState;
+  readonly otherPlayerResponder: (board: Board) => Promise<GameState>;
 };
 
-export default function StatefulGameCard(): ReactElement {
+export default function StatefulGameCard({
+  initialGameState,
+  otherPlayerResponder,
+}: Props): ReactElement {
   const [gameStates, setGameStates] = useState<GameStates>({ currentState: initialGameState });
+
+  const clickCellCallback = (board: Board, move: Move): void => {
+    const newBoard = makeMove(board, move);
+    if (newBoard === null) {
+      setGameStates((previousState) => ({
+        ...previousState,
+        currentState: { ...previousState.currentState, status: 'ILLEGAL_MOVE' },
+      }));
+      return;
+    }
+    const gameStatus = getGameStatus(newBoard);
+    let newStatus: GameStatus;
+    if (gameStatus === 1) {
+      newStatus = 'BLACK_WINS';
+    } else if (gameStatus === -1) {
+      newStatus = 'WHITE_WINS';
+    } else {
+      newStatus = 'AI_MOVE';
+    }
+    setGameStates((previousState) => ({
+      previousState: {
+        ...previousState,
+        currentState: { ...previousState.currentState, status: 'PLAYER_MOVE' },
+      },
+      currentState: { board: newBoard, highlightedCell: move, status: newStatus },
+    }));
+    otherPlayerResponder(newBoard).then((currentState) =>
+      setGameStates((previousState) => ({ ...previousState, currentState }))
+    );
+  };
 
   return (
     <GameCard
@@ -103,9 +76,7 @@ export default function StatefulGameCard(): ReactElement {
           gameStates.currentState.status === 'ILLEGAL_MOVE') &&
         gameStates.previousState != null
       }
-      clickCallback={(a, b) =>
-        clickCellCallback(gameStates.currentState.board, [a, b], setGameStates, aiResponder)
-      }
+      clickCallback={(a, b) => clickCellCallback(gameStates.currentState.board, [a, b])}
       onSelectSide={(id) => {
         const newBoard = id === 1 ? emptyBoard : makeMoveWithoutCheck(emptyBoard, [4, 4]);
         setGameStates({ currentState: { board: newBoard, status: 'PLAYER_MOVE' } });
