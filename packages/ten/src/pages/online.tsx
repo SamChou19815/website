a93f4f404bdd44d944e0d3739986f217c197a954
Sam <sam@developersam.com>
2020-11-23 14:12:34 -0500

    [ten] Setup some crappy online game logic
    The code here is definitely wrong...

--- packages/ten/src/pages/online.tsx
+++ packages/ten/src/pages/online.tsx
@@ -3,7 +3,6 @@ import React, { ReactElement, Suspense, lazy } from 'react';
 import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 
 import App from '../components/App';
-import OnlineGameCard from '../components/OnlineGameCard';
 
 import './index.css';
 import LoadingOverlay from 'lib-react/LoadingOverlay';
@@ -11,6 +10,7 @@ import LoadingOverlay from 'lib-react/LoadingOverlay';
 // Lazy import is necessary for conditional execution below,
 // since firebase/app code cannot be executed in SSR environments.
 const FirebaseLoginAppBarrier = lazy(() => import('../components/FirebaseLoginBarrier'));
+const OnlineGameCard = lazy(() => import('../components/OnlineGameCard'));
 
 export default function Online(): ReactElement {
   return (
