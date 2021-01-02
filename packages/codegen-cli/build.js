931e284f71e707924830ebadc566cae6ac827082
Sam Zhou <sam@developersam.com>
2021-01-01 20:15:54 -0500

    [repo] Inline bundler code into codegen-cli (#1070)

--- packages/codegen-cli/build.js
+++ packages/codegen-cli/build.js
@@ -1,11 +1,8 @@
 #!/usr/bin/env node
 
-// @ts-check
-
 const { chmodSync, mkdirSync, writeFileSync } = require('fs');
 const { dirname, join, resolve } = require('path');
 
-// @ts-expect-error: no typedef :(
 const ncc = require('@vercel/ncc');
 
 ncc(join(resolve('.'), 'src', 'index.ts'), {
@@ -13,7 +10,6 @@ ncc(join(resolve('.'), 'src', 'index.ts'), {
   sourceMapRegister: false,
   transpileOnly: true,
   quiet: true,
-  externals: ['typescript'],
 }).then((/** @type {{code: string}} */ { code }) => {
   const outputPath = join('bin', 'index');
   mkdirSync(dirname(outputPath), { recursive: true });
