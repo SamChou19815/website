526b03ac1e88afc36bad892f2ec4cd994cd38af6
Sam <sam@developersam.com>
2021-01-01 23:28:37 -0500

    [repo] Make yarn plugin take the responsibility of codegen

---
+++ packages/yarn-workspaces-json-types/index.d.ts
@@ -0,0 +1,10 @@
+export type YarnInvididualWorkspaceInformation = {
+  readonly workspaceLocation: string;
+  readonly dependencyChain: readonly string[];
+};
+
+export type YarnWorkspacesJson = {
+  readonly __type__: unknown;
+  readonly information: Readonly<Record<string, YarnInvididualWorkspaceInformation>>;
+  readonly topologicallyOrdered: readonly string[];
+};
