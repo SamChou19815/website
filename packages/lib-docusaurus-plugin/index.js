cc80c3b7981002adc8b7c611fcdce0a00c60d6af
Sam Zhou <sam@developersam.com>
2021-03-11 16:19:19 -0500

    [docusaurus] Use esbuild for slightly better build performance (#1109)

--- packages/lib-docusaurus-plugin/index.js
+++ packages/lib-docusaurus-plugin/index.js
@@ -1,5 +1,26 @@
 const path = require('path');
 
+const { ESBuildPlugin, ESBuildMinifyPlugin } = require('esbuild-loader');
+const { ProvidePlugin } = require('webpack');
+
+const withEsbuildMinify = (config) => {
+  if (!config.optimization.minimizer) return;
+  const terserIndex = config.optimization.minimizer.findIndex(
+    (minimizer) => minimizer.constructor.name === 'TerserPlugin'
+  );
+  if (terserIndex > -1) {
+    config.optimization.minimizer.splice(terserIndex, 1, new ESBuildMinifyPlugin());
+  }
+};
+
+const withEsbuildLoader = (config) => {
+  const jsLoader = config.module.rules.find((rule) => rule.test && rule.test.test('.js'));
+  if (jsLoader) {
+    jsLoader.use.loader = require.resolve('esbuild-loader');
+    jsLoader.use.options = { loader: 'tsx', target: 'es2017' };
+  }
+};
+
 const setupPlugin = () => ({
   name: 'lib-docusaurus-plugin',
   getThemePath() {
@@ -7,6 +28,12 @@ const setupPlugin = () => ({
   },
   getClientModules() {
     return [require.resolve('lib-react/PrismCodeBlock.css')];
+  },
+  configureWebpack(config) {
+    config.plugins.push(new ESBuildPlugin(), new ProvidePlugin({ React: 'react' }));
+    withEsbuildMinify(config);
+    withEsbuildLoader(config);
+    return true;
   },
 });
 
