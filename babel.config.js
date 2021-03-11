69416efd5fbdd5e1fbe82b0d9bb6aff6922a4378
Sam Zhou <sam@developersam.com>
2021-03-11 12:20:46 -0500

    [dependency] Bump infra packages (#1107)

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,3 @@
-// Exists in order to use jest.
 module.exports = {
-  presets: ['next/babel'],
+  presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
 };
