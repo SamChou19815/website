c1c99e36848de56d5230bb925a943fbc60fbdb0f
Sam <sam@developersam.com>
2021-03-28 21:09:42 -0400

    [repo] Use esbuild runner for tests

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-const babelJestPath = require.resolve('babel-jest');
-
-module.exports = {
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': babelJestPath,
-  },
-  transformIgnorePatterns: ['^.+\\.module\\.(css|sass|scss)$'],
-};
