3195e09a22525eef6e001c19e431c871de4a3fcc
Sam Zhou <sam@developersam.com>
2021-04-12 22:56:30 -0400

    [dot-dev] Simplify website (#1129)

--- packages/dot-dev/components/data.ts
+++ packages/dot-dev/components/data.ts
@@ -4,7 +4,6 @@ export type FanArtWork = {
   readonly link: string;
   readonly author: Author;
   readonly description: string;
-  readonly pageLink: string;
 };
 
 export const MEGAN: Author = { authorName: 'Megan Yin', authorWebsite: 'https://meganyin.com/' };
@@ -14,33 +13,28 @@ export const FAN_ART_ITERATION_0: FanArtWork = {
   link: '/fan-arts/dev-sam-fan-art-0.webp',
   author: PETER,
   description: '@dev-sam/fan-art Iteration 0',
-  pageLink: '/fan-art-iteration-0',
 };
 
 export const FAN_ART_ITERATION_1: FanArtWork = {
   link: '/fan-arts/dev-sam-fan-art-1.webp',
   author: MEGAN,
   description: '@dev-sam/fan-art Iteration 1',
-  pageLink: '/fan-art-iteration-1',
 };
 
 export const FAN_ART_ITERATION_2: FanArtWork = {
   link: '/fan-arts/dev-sam-fan-art-2.webp',
   author: MEGAN,
   description: '@dev-sam/fan-art Iteration 2',
-  pageLink: '/fan-art-iteration-2',
 };
 
 export const FAN_ART_ITERATION_3: FanArtWork = {
   link: '/fan-arts/dev-sam-fan-art-3.webp',
   author: MEGAN,
   description: '@dev-sam/fan-art Iteration 3',
-  pageLink: '/fan-art-iteration-3',
 };
 
 export const FAN_ART_BIRTHDAY_EDITION: FanArtWork = {
   link: '/fan-arts/dev-sam-birthday-edition.webp',
   author: MEGAN,
   description: '@dev-sam/fan-art Birthday Edition',
-  pageLink: '/fan-art-birthday-edition',
 };
--- packages/dot-dev/pages/index.tsx
+++ packages/dot-dev/pages/index.tsx
@@ -1,6 +1,7 @@
-import type { ReactElement } from 'react';
+import Head from 'next/head';
+import React, { ReactElement } from 'react';
 
-import FanArtWorkPage from '../components/FanArtWorkPage';
+import FanArtWorkCard from '../components/FanArtWorkCard';
 import {
   FAN_ART_ITERATION_0,
   FAN_ART_ITERATION_1,
@@ -10,16 +11,30 @@ import {
 } from '../components/data';
 
 const ArtsPage = (): ReactElement => (
-  <FanArtWorkPage
-    title="Fan Arts"
-    works={[
-      FAN_ART_BIRTHDAY_EDITION,
-      FAN_ART_ITERATION_3,
-      FAN_ART_ITERATION_2,
-      FAN_ART_ITERATION_1,
-      FAN_ART_ITERATION_0,
-    ]}
-  />
+  <div>
+    <Head>
+      <title>Fan Arts | Random@dev-sam</title>
+    </Head>
+    <nav className="navbar">
+      <div className="navbar__inner">
+        <div className="navbar__items">
+          <a className="navbar__brand" href="/">
+            Fan Arts | Random@dev-sam
+          </a>
+        </div>
+        <div className="navbar__items navbar__items--right">
+          <a className="navbar__item navbar__link" href="https://developersam.com">
+            Home
+          </a>
+        </div>
+      </div>
+    </nav>
+    <FanArtWorkCard {...FAN_ART_BIRTHDAY_EDITION} />
+    <FanArtWorkCard {...FAN_ART_ITERATION_3} />
+    <FanArtWorkCard {...FAN_ART_ITERATION_2} />
+    <FanArtWorkCard {...FAN_ART_ITERATION_1} />
+    <FanArtWorkCard {...FAN_ART_ITERATION_0} />
+  </div>
 );
 
 export default ArtsPage;
