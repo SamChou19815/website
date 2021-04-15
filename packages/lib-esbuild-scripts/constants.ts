f59871ac5614d2a4b30bcc4284579ab49db6d9a9
Sam <sam@developersam.com>
2021-04-14 20:50:50 -0400

    [esbuild-scripts] Support typescript entrypoints

--- packages/lib-esbuild-scripts/constants.ts
+++ packages/lib-esbuild-scripts/constants.ts
@@ -1,7 +1,7 @@
 import { join } from 'path';
 
-export const CLIENT_ENTRY = join(__dirname, 'client.js');
-export const SERVER_ENTRY = join(__dirname, 'server.js');
+export const CLIENT_ENTRY = join(__dirname, 'entries', 'client.ts');
+export const SERVER_ENTRY = join(__dirname, 'entries', 'server.ts');
 export const SSR_JS_PATH = join('build', '__ssr.js');
 export const SSR_CSS_PATH = join('build', '__ssr.css');
 export const BUILD_HTML_PATH = join('build', 'index.html');
