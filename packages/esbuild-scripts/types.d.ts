089ceb4fdf879b4baca4478fef4a51e850386f40
Sam <sam@developersam.com>
2021-04-20 00:13:54 -0400

    [esbuild-scripts] Support resource imports

--- packages/esbuild-scripts/types.d.ts
+++ packages/esbuild-scripts/types.d.ts
@@ -1,9 +1,29 @@
-declare module '*.module.css' {
-  const classes: { readonly [key: string]: string };
-  export default classes;
+declare module '*.jpg' {
+  const src: string;
+  export default src;
 }
 
-declare module '*.css' {
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.gif' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.webp' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.pdf' {
   const src: string;
   export default src;
 }
