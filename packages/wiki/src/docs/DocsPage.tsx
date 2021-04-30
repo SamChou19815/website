ff12b98f92b834ef3cd38cc98d0799c4ad6882bc
Sam <sam@developersam.com>
2021-04-29 22:17:59 -0400

    [wiki] Correct titles

--- packages/wiki/src/docs/DocsPage.tsx
+++ packages/wiki/src/docs/DocsPage.tsx
@@ -6,18 +6,23 @@ import documentSideBars from './document-sidebars';
 import Head from 'esbuild-scripts/components/Head';
 import { useLocation } from 'esbuild-scripts/components/router-hooks';
 
-const DocsPage = ({ children }: { readonly children: ReactNode }): ReactElement => (
-  <div className="top-level">
-    <Head>
-      <title>Docs | Wiki</title>
-    </Head>
-    <div className="sidebar-container">
-      <DocSidebar sidebar={documentSideBars} activePath={useLocation().pathname} />
+const DocsPage = ({ children }: { readonly children: ReactNode }): ReactElement => {
+  const activePath = useLocation().pathname;
+  const title = documentSideBars.find((it) => it.href === activePath)?.label ?? 'Docs';
+
+  return (
+    <div className="top-level">
+      <Head>
+        <title>{title} | Wiki</title>
+      </Head>
+      <div className="sidebar-container">
+        <DocSidebar sidebar={documentSideBars} activePath={activePath} />
+      </div>
+      <main className="container document-container">
+        <div className="container padding-vert--lg">{children}</div>
+      </main>
     </div>
-    <main className="container document-container">
-      <div className="container padding-vert--lg">{children}</div>
-    </main>
-  </div>
-);
+  );
+};
 
 export default DocsPage;
