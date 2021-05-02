037ee5b8b299d172fb61cf133bfd9af98829bb5e
Sam <sam@developersam.com>
2021-05-01 22:33:23 -0400

    [wiki] Factor out DocNavBar

--- packages/wiki/src/pages/_document.tsx
+++ packages/wiki/src/pages/_document.tsx
@@ -1,9 +1,7 @@
-import clsx from 'clsx';
 import React, { ReactElement, ReactNode } from 'react';
 
 import Head from 'esbuild-scripts/components/Head';
-import Link from 'esbuild-scripts/components/Link';
-import { useLocation } from 'esbuild-scripts/components/router-hooks';
+import DocNavBar from 'lib-react-docs/DocNavBar';
 
 import 'infima/dist/css/default/default.min.css';
 import 'lib-react-prism/PrismCodeBlock.css';
@@ -12,7 +10,6 @@ import './index.css';
 import './app.scss';
 
 const Document = ({ children }: { readonly children: ReactNode }): ReactElement => {
-  const path = useLocation().pathname;
   return (
     <>
       <Head>
@@ -29,40 +26,14 @@ const Document = ({ children }: { readonly children: ReactNode }): ReactElement 
         />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Mono:400,500" />
       </Head>
-      <nav className="navbar navbar--fixed-top">
-        <div className="navbar__inner">
-          <div className="navbar__items">
-            <Link className="navbar__brand" to="/">
-              <img
-                className="navbar__logo"
-                src="https://developersam.com/logo.png"
-                alt="Developer Sam logo"
-              />
-              <strong className="navbar__title">Wiki</strong>
-            </Link>
-            <Link
-              className={clsx(
-                'navbar__item',
-                'navbar__link',
-                path.startsWith('/docs') && 'navbar__link--active'
-              )}
-              to="/docs/intro"
-            >
-              Docs
-            </Link>
-            <Link
-              className={clsx(
-                'navbar__item',
-                'navbar__link',
-                path === '/intern' && 'navbar__link--active'
-              )}
-              to="/intern"
-            >
-              internals@dev-sam
-            </Link>
-          </div>
-        </div>
-      </nav>
+      <DocNavBar
+        title="Wiki"
+        logo="https://developersam.com/logo.png"
+        logoName="Developer Sam logo"
+        githubLink="https://github.com/SamChou19815/website"
+        firstDocumentLink="/docs/intro"
+        otherLinks={[{ name: 'internals@dev-sam', link: '/intern' }]}
+      />
       <div className="main-wrapper">{children}</div>
     </>
   );
