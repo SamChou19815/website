f392c48cd36e6abb1e69b94f94120c3be6af94b9
Sam Zhou <sam@developersam.com>
2021-08-01 10:53:34 -0700

    [www] Fix samlang program syntax after parser change (#1304)

--- packages/www/src/components/StickyCodeBlock.tsx
+++ packages/www/src/components/StickyCodeBlock.tsx
@@ -20,11 +20,11 @@ class List<T>(Nil(unit), Cons([T * List<T>])) {
 }
 class Developer(
   val name: string, val github: string,
-  val projects: List<string>,
+  val projects: List<string>
 ) {
   function sam(): Developer = {
-    val l = List.of("samlang").cons("...")
-    val github = "SamChou19815"
+    val l = List.of("samlang").cons("...");
+    val github = "SamChou19815";
     { name: "Sam Zhou", github, projects: l }
   }
 }
