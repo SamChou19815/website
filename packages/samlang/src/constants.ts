5e6c47a7b32d3e5fd55b7c5bde4742617e93e03a
Sam Zhou <sam@developersam.com>
2021-09-23 12:19:55 -0400

    [samlang] Remove outdated docs (#1344)

--- packages/samlang/src/constants.ts
+++ packages/samlang/src/constants.ts
@@ -2,9 +2,9 @@ export const SAMLANG_TITLE = 'samlang';
 export const SAMLANG_LOGO = '/img/favicon.png';
 export const SAMLANG_URL = 'https://samlang.io';
 
-type SimpleSidebarItems = readonly string[] | { readonly [category: string]: SimpleSidebarItems };
-
-export const SAMLANG_SIDEBAR_ITEMS: SimpleSidebarItems = {
-  'Language Basics': ['/introduction', '/classes-types', '/expressions', '/type-inference'],
-  'Implementation Notes': ['/architecture', '/intermediate-representation'],
-};
+export const SAMLANG_SIDEBAR_ITEMS: readonly string[] = [
+  '/introduction',
+  '/classes-types',
+  '/expressions',
+  '/type-inference',
+];
