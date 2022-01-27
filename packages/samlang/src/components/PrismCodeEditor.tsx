4561464f9cdd36fdb628a4d93ed04bf9b518810d
Sam Zhou <sam@developersam.com>
2022-01-26 17:15:59 -0800

    [samlang] Update samlang and setup language service

--- packages/samlang/src/components/PrismCodeEditor.tsx
+++ packages/samlang/src/components/PrismCodeEditor.tsx
@@ -1,14 +1,20 @@
 // Credit: Adapted from https://github.com/satya164/react-simple-code-editor/blob/master/src/index.js
 
-import { createSamlangLanguageService } from '@dev-sam/samlang-core';
+import createSamlangLanguageService from '@dev-sam/samlang-core/services';
 import Editor from '@monaco-editor/react';
 import React, { useRef } from 'react';
-import { initializeMonacoEditor, monacoEditorOptions } from './monaco-editor-config';
+import { DemoModuleReference, initializeMonacoEditor, monacoEditorOptions } from './samlang-config';
 
 type Props = { readonly code: string; readonly onCodeChange: (value: string) => void };
 
 export default function PrismCodeEditor({ code, onCodeChange }: Props): JSX.Element {
   const serviceRef = useRef(createSamlangLanguageService([]));
+
+  function onChange(newCode?: string) {
+    if (newCode == null) return;
+    onCodeChange(newCode);
+    serviceRef.current.state.update(DemoModuleReference, newCode);
+  }
 
   return (
     <Editor
@@ -17,7 +23,7 @@ export default function PrismCodeEditor({ code, onCodeChange }: Props): JSX.Elem
       height="400px"
       value={code}
       options={monacoEditorOptions}
-      onChange={(newCode) => newCode && onCodeChange(newCode)}
+      onChange={onChange}
       beforeMount={(monaco) => initializeMonacoEditor(monaco, serviceRef.current)}
     />
   );
