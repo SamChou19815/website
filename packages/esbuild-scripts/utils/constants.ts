d04217c15ec7da4557b5d9f7d0e07391b39e7dc1
Sam Zhou <sam@developersam.com>
2022-06-12 11:27:15 -0700

    [repo] Switch all product packages to ESM

--- packages/esbuild-scripts/utils/constants.ts
+++ packages/esbuild-scripts/utils/constants.ts
@@ -1,10 +1,10 @@
 import * as path from 'path';
 
 export const PAGES_PATH = path.join('src', 'pages');
-export const SSR_JS_PATH = path.join('build', '__ssr.cjs');
+export const SSR_JS_PATH = path.join('build', '__ssr.mjs');
 export const SSR_CSS_PATH = path.join('build', '__ssr.css');
 export const BUILD_PATH = 'build';
 export const OUT_PATH = 'out';
 export const VIRTUAL_PATH_PREFIX = 'esbuild-scripts-internal/virtual/';
 export const VIRTUAL_GENERATED_ENTRY_POINT_PATH_PREFIX = `${VIRTUAL_PATH_PREFIX}__generated-entry-point__/`;
-export const VIRTUAL_SERVER_ENTRY_PATH = `${VIRTUAL_GENERATED_ENTRY_POINT_PATH_PREFIX}__server__.cjs`;
+export const VIRTUAL_SERVER_ENTRY_PATH = `${VIRTUAL_GENERATED_ENTRY_POINT_PATH_PREFIX}__server__.mjs`;
