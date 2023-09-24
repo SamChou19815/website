64bdd75bd299ade6d85b9ae7b416e6589deefef5
Sam Zhou <sam@developersam.com>
2023-09-24 13:57:52 -0700

    [tools] Add schema validation util

--- packages/private-tools/lib/any-flavors.d.ts
+++ packages/private-tools/lib/any-flavors.d.ts
@@ -1,2 +1,5 @@
 // biome-ignore lint/suspicious/noExplicitAny: any flavor
 export type JsonAny = any;
+
+// biome-ignore lint/suspicious/noExplicitAny: any flavor
+export type ConditionalTypeAny = any;
---
+++ packages/private-tools/lib/schema.ts
@@ -0,0 +1,105 @@
+import type { ConditionalTypeAny } from "./any-flavors";
+
+export type FieldSpec =
+  | { readonly kind: "unknown" }
+  | { readonly kind: "boolean" }
+  | { readonly kind: "number" }
+  | { readonly kind: "string" }
+  | { readonly kind: "enum"; readonly exact: string }
+  | { readonly kind: "array"; readonly nested: FieldSpec }
+  | { readonly kind: "object"; readonly nested: { readonly [field: string]: FieldSpec } }
+  | { readonly kind: "union"; readonly variants: readonly FieldSpec[] };
+
+export type FieldSpecToType<T extends FieldSpec> = T extends { readonly kind: "unknown" }
+  ? unknown
+  : T extends { readonly kind: "boolean" }
+  ? boolean
+  : T extends { readonly kind: "number" }
+  ? number
+  : T extends { readonly kind: "string" }
+  ? string
+  : T extends { readonly kind: "enum"; exact: string }
+  ? T["exact"]
+  : T extends { readonly kind: "array"; readonly nested: FieldSpec }
+  ? ReadonlyArray<FieldSpecToType<T["nested"]>>
+  : T extends { readonly kind: "object"; readonly nested: { readonly [field: string]: FieldSpec } }
+  ? { readonly [K in keyof T["nested"]]: FieldSpecToType<T["nested"][K]> }
+  : T extends { readonly kind: "union"; readonly variants: readonly FieldSpec[] }
+  ? FieldSpecToType<T["variants"][number]>
+  : never;
+
+export function objectSchema<const O extends { readonly [field: string]: FieldSpec }>(nested: O): {
+  readonly kind: "object";
+  nested: O;
+} {
+  return { kind: "object", nested };
+}
+
+export function defineSchema<const Spec extends FieldSpec>(spec: Spec): Spec {
+  return spec;
+}
+
+function validateAccordingToSchemaUntyped<const Spec extends FieldSpec>(
+  spec: Spec,
+  unvalidated: unknown,
+): void {
+  switch (spec.kind) {
+    case "unknown":
+      return;
+    case "boolean":
+      if (typeof unvalidated === "boolean") {
+        return;
+      }
+      throw `Expected boolean, but got ${unvalidated}`;
+    case "number":
+      if (typeof unvalidated === "number") {
+        return;
+      }
+      throw `Expected number, but got ${unvalidated}`;
+    case "string":
+      if (typeof unvalidated === "string") {
+        return;
+      }
+      throw `Expected string, but got ${unvalidated}`;
+    case "enum":
+      if (spec.exact === unvalidated) {
+        return;
+      }
+      throw `Expected "${spec.exact}", but got ${unvalidated}`;
+    case "array":
+      if (Array.isArray(unvalidated)) {
+        unvalidated.forEach((it) => validateAccordingToSchemaUntyped(spec.nested, it));
+        return;
+      }
+      throw `Expected array, but got ${unvalidated}`;
+    case "object":
+      if (unvalidated != null && typeof unvalidated === "object") {
+        Object.entries(unvalidated).forEach(([key, value]) => {
+          const nestedSpec = spec.nested[key];
+          if (nestedSpec != null) {
+            validateAccordingToSchemaUntyped(nestedSpec, value);
+          }
+        });
+        return;
+      }
+      throw `Expected array, but got ${unvalidated}`;
+    case "union":
+      for (const candidateSpec of spec.variants) {
+        try {
+          validateAccordingToSchemaUntyped(candidateSpec, unvalidated);
+          return;
+        } catch {}
+      }
+      throw "Cannot match any variant.";
+    default:
+      spec satisfies never;
+  }
+}
+
+export function validateAccordingToSchema<const Spec extends FieldSpec>(
+  spec: Spec,
+  unvalidated: unknown,
+): FieldSpecToType<Spec> {
+  validateAccordingToSchemaUntyped(spec, unvalidated);
+  return unvalidated as ConditionalTypeAny;
+}
